18041558c82f857fd9be87740d1271381366e497
Ashutosh <ashutosh@onepercentclub.com>
2017-09-17 16:47:29 +0200

    add microsoft mobile center integration

---
+++ ios/Pods/Target Support Files/Pods-cruijff/Pods-cruijff-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_cruijff : NSObject
+@end
+@implementation PodsDummy_Pods_cruijff
+@end
---
+++ ios/Pods/Target Support Files/Pods-cruijffTests/Pods-cruijffTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_cruijffTests : NSObject
+@end
+@implementation PodsDummy_Pods_cruijffTests
+@end
