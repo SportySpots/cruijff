97ea7659b2ccf513f3ab2c6f4a3dade8c6797d7b
Ashutosh <ashutosh@onepercentclub.com>
2017-09-22 16:02:48 +0200

    reorganize imports

--- App/Containers/App.js
+++ App/Containers/App.js
@@ -1,10 +1,9 @@
-import '../Config'
-import DebugConfig from '../Config/DebugConfig'
-
 import React, {Component} from 'react'
 import {Provider} from 'react-redux'
 import {ThemeProvider} from 'react-native-material-ui'
 
+import '../Config'
+import DebugConfig from '../Config/DebugConfig'
 import RootContainer from './RootContainer'
 import createStore from '../Redux'
 
