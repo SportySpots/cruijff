3b56c39864cb2c2160ed450d2deef3b78b673ee9
ashutosh <ashutoshb@gmail.com>
2018-02-10 15:06:40 +0100

    Add location permission screen

--- App/Containers/OnboardingScreen.js
+++ App/Containers/OnboardingScreen.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Image } from 'react-native'
 import Onboarding from '../Components/Onboarding'
 import Images from '../Themes/Images'
+import Colors from '../Themes/Colors'
 
 export default class OnboardingScreen extends Component {
   render () {
@@ -10,29 +11,29 @@ export default class OnboardingScreen extends Component {
       <Onboarding
         pages={[
           {
-            backgroundColor: '#fff',
+            backgroundColor: Colors.secondaryGreen,
             image: <Image source={Images.illustrationWizard1} />,
             title: 'Hi Sport',
             subtitle:
               'Thanks for joining the yet to become biggest free sportclub in the world. \n Happy sporting!'
           },
           {
-            backgroundColor: '#fff',
+            backgroundColor: Colors.secondaryGreen,
             image: <Image source={Images.illustrationWizard2} />,
             title: 'Join a Game',
             subtitle:
               "Browse the list all Sportyspots near you. Tap the 'Join a game' icon to find organized games near you"
           },
           {
-            backgroundColor: '#fff',
+            backgroundColor: Colors.secondaryGreen,
             image: <Image source={Images.illustrationWizard3} />,
             title: 'Plan a Game',
             subtitle:
               "Tap the 'Plan a game' icon to organize a game of your favorite sport with your friends or other players"
           }
         ]}
-        onSkip={() => navigate('DefaultNav')}
-        onDone={() => navigate('DefaultNav')}
+        onSkip={() => navigate('LocationPermissionScreen')}
+        onDone={() => navigate('LocationPermissionScreen')}
       />
     )
   }
