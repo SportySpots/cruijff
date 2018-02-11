5f3351812643da06fdef5e5f12232f4ae2f6afe3
ashutosh <ashutoshb@gmail.com>
2018-02-11 17:05:08 +0100

    cleanup styles

--- App/Themes/ApplicationStyles.js
+++ App/Themes/ApplicationStyles.js
@@ -10,7 +10,7 @@ const ApplicationStyles = {
     mainContainer: {
       flex: 1,
       flexDirection: 'column',
-      backgroundColor: Colors.actionBlue
+      backgroundColor: Colors.white
     },
     container: {
       flex: 1,
@@ -46,23 +46,6 @@ const ApplicationStyles = {
       fontSize: 14,
       color: Colors.text
     }
-  },
-  darkLabelContainer: {
-    borderBottomWidth: 1,
-    borderBottomColor: Colors.border,
-    padding: Metrics.smallMargin,
-    paddingBottom: Metrics.doubleBaseMargin,
-    marginBottom: Metrics.baseMargin
-  },
-  darkLabel: {
-    fontFamily: Fonts.type.bold,
-    color: Colors.snow
-  },
-  groupContainer: {
-    margin: Metrics.smallMargin,
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center'
   }
 }
 
--- App/Themes/Metrics.js
+++ App/Themes/Metrics.js
@@ -2,7 +2,6 @@ import { Dimensions, Platform } from 'react-native'
 
 const { width, height } = Dimensions.get('window')
 
-// Used via Metrics.baseMargin
 const metrics = {
   smallMargin: 5,
   baseMargin: 10,
