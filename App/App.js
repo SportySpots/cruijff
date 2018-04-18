32b16a20e8c54e9cc63819d024442aa33406897b
Tom Klaver <tomklav@gmail.com>
2018-04-18 17:00:36 +0200

    adding graphql

--- App/App.js
+++ App/App.js
@@ -5,6 +5,41 @@ import { Provider } from 'react-redux'
 import RootContainer from './Containers/RootContainer'
 import createStore from './Redux'
 
+import ApolloClient from 'apollo-boost'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import gql from 'graphql-tag'
+import { graphql, buildClientSchema } from 'graphql'
+import { AsyncStorage } from 'react-native'
+import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools'
+import faker from 'faker'
+
+window.setTimeout(async () => {
+  const token = await AsyncStorage.getItem('TOKEN', console.log)
+  const client = new ApolloClient({
+    uri: 'http://127.0.0.1:8000/graphql',
+    cache: new InMemoryCache()
+  })
+  const schema = buildClientSchema(require('../schema.graphql.json'))
+  addMockFunctionsToSchema({
+    schema,
+    mocks: {
+      UUID: () => faker.random.uuid()
+    }
+  })
+
+  const query = `
+      {
+          spots {
+            uuid, 
+            spotGames { 
+              uuid 
+            }
+          },
+        }
+    `
+  graphql(schema, query).then(result => console.log('Got result', result))
+}, 1000)
+
 // create our store
 const store = createStore()
 console.log(store.getState())
