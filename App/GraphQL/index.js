89e089f7213dc2fef991f2ae8cc9e7b7020005a1
Tom Klaver <tomklav@gmail.com>
2018-04-19 09:24:10 +0200

    copy apollo-boost configuration to ApolloClient initialization

--- App/GraphQL/index.js
+++ App/GraphQL/index.js
@@ -13,6 +13,8 @@ import DebugConfig from '../Config/DebugConfig'
 import { SchemaLink } from 'apollo-link-schema'
 import gql from 'graphql-tag'
 import { ApolloLink } from 'apollo-link'
+import { onError } from 'apollo-link-error'
+import { HttpLink } from 'apollo-boost'
 
 export const createClient = async () => {
   if (DebugConfig.useFixtures) {
@@ -20,15 +22,24 @@ export const createClient = async () => {
   }
 
   const token = await AsyncStorage.getItem('TOKEN')
-
   return new ApolloClient({
-    uri: 'http://127.0.0.1:8000/graphql',
-    cache: new InMemoryCache(),
-    fetchOptions: {
-      headers: {
-        authorization: `Bearer ${token}`
-      }
-    }
+    link: ApolloLink.from([
+      onError(({ graphQLErrors, networkError }) => {
+        if (graphQLErrors) {
+          graphQLErrors.map(({ message, locations, path }) =>
+            console.log(
+              `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+            )
+          )
+        }
+        if (networkError) console.log(`[Network error]: ${networkError}`)
+      }),
+      new HttpLink({
+        uri: 'http://127.0.0.1:8000/graphql',
+        headers: { authorization: `Bearer ${token}` }
+      })
+    ]),
+    cache: new InMemoryCache()
   })
 }
 
