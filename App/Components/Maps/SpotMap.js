dd805f2bb2dde377ff06cdba9b3f7f98d4417574
fede-rodes <rodesf@gmail.com>
2018-04-29 14:45:02 +0200

    fixing lint errors

--- App/Components/Maps/SpotMap.js
+++ App/Components/Maps/SpotMap.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import { TouchableOpacity, View } from 'react-native'
+import { View } from 'react-native' // eslint-disable-line
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import Icon from 'react-native-vector-icons/MaterialIcons'
-import MapView from 'react-native-maps'
-import { Marker } from 'react-native-maps'
+import MapView, { Marker } from 'react-native-maps'
 import { showLocation } from 'react-native-map-link'
 import Colors from '../../Themes/Colors'
 import RoundButton from '../RoundButton'
