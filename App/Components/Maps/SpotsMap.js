88fd5e3aa13882b479c360a6d64e8e7201f966fd
fede-rodes <rodesf@gmail.com>
2018-05-01 23:42:28 +0200

    fix error on handleRegionChange

--- App/Components/Maps/SpotsMap.js
+++ App/Components/Maps/SpotsMap.js
@@ -80,7 +80,10 @@ class SpotsMap extends React.PureComponent {
 
   // Fires every time the map stops panning
   handleRegionChange = region => {
-    this.setState(region)
+    const { latitude, longitude, latitudeDelta, longitudeDelta } = region
+    const center = { latitude, longitude }
+    const delta = { latitudeDelta, longitudeDelta }
+    this.setState({ center, delta })
   }
 
   // Fires every time a marker is clicked in order to swipe the the card
@@ -187,3 +190,179 @@ SpotsMap.defaultProps = {
 }
 
 export default SpotsMap
+
+/* import React from 'react'
+import { TouchableOpacity, View, Dimensions } from 'react-native' // eslint-disable-line
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import MapView, { Marker } from 'react-native-maps'
+import Swiper from 'react-native-swiper'
+import Colors from '../../Themes/Colors'
+import { cardList } from '../Spots/Styles/CardStyles'
+import CardSmall from '../Spots/SpotMapCardSmall'
+
+// TODO: display google-maps link in case the map component crashes.
+// Use <ErrorBoundary>
+
+const { width, height } = Dimensions.get('window')
+const ASPECT_RATIO = width / height
+const LATITUDE_DELTA = 50 // 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO
+
+const Relative = styled.View`
+  position: relative;
+  margin: 0;
+`
+
+const Absolute = styled.View`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+`
+
+const CardContainer = ({ onPress, ...rest }) => (
+  <TouchableOpacity onPress={onPress} style={cardList.cardContainer}>
+    <CardSmall {...rest} />
+  </TouchableOpacity>
+)
+
+class SpotsMap extends React.PureComponent {
+  state = {
+    region: {
+      latitude: this.props.spots[0].address.lat,
+      longitude: this.props.spots[0].address.lng,
+      latitudeDelta: this.props.delta.latitudeDelta,
+      longitudeDelta: this.props.delta.latitudeDelta
+    },
+    currentSpot: 0 // index
+  }
+
+  // Fires every time a card is swiped. The new index is passed as the argument
+  handleIndexChange = index => {
+    // Get spot associated to the new index
+    const spot = this.props.spots[index]
+    // Get spot location
+    const { address: { lat, lng } } = spot
+    // Center map on the spot
+    const center = { latitude: lat, longitude: lng }
+    this.setState({
+      currentSpot: index,
+      region: { ...this.state.region, ...center }
+    })
+  }
+
+  // Fires every time the map stops panning
+  handleRegionChange = region => {
+    console.log('REGION', region)
+    this.setState({ region })
+  }
+
+  // Fires every time a marker is clicked in order to swipe the the card
+  // associated to said marked into the view port
+  handleMarkerPess = index => {
+    const { region, currentSpot } = this.state // eslint-disable-line
+    // Calculate the offset between the index of the current spot and the new one
+    const offset = index - currentSpot
+    // Get spot associated to the new index
+    const spot = this.props.spots[index]
+    // Get spot location
+    const { address: { lat, lng } } = spot
+    // Center map on the spot
+    const center = { latitude: lat, longitude: lng } // eslint-disable-line
+    // Make sure the card is swiped after the currentSpot is updated (otherwise
+    // we'll have an infinite loop!)
+    this.setState(({
+      currentSpot: index,
+      region: { ...region, ...center }
+    }) => {
+      this.refs.swiper.scrollBy(offset, true)
+    })
+  }
+
+  render () {
+    const { navigation, spots } = this.props
+    const { region, currentSpot } = this.state
+
+    return (
+      <Relative style={{ flex: 1 }}>
+        <MapView
+          style={{ flex: 1 }}
+          region={region}
+          moveOnMarkerPress={false}
+          onRegionChangeComplete={this.handleRegionChange}
+        >
+          {spots.map(({ uuid, address, name }, index) => {
+            const center = { latitude: address.lat, longitude: address.lng }
+            return (
+              <Marker
+                key={name}
+                tracksViewChanges={false}
+                coordinate={center}
+                pinColor={
+                  currentSpot === index ? Colors.primaryGreen : Colors.black
+                }
+                onPress={() => {
+                  this.handleMarkerPess(index)
+                }}
+              />
+            )
+          })}
+        </MapView>
+        <Absolute>
+          <Swiper
+            ref='swiper'
+            loop={false}
+            bounces
+            showsPagination={false}
+            containerStyle={{ height: 80 }}
+            index={currentSpot}
+            onIndexChanged={this.handleIndexChange}
+          >
+            {spots.map(spot => (
+              <CardContainer
+                key={spot.uuid}
+                spot={spot}
+                onPress={() =>
+                  navigation.navigate('SpotDetailsScreen', {
+                    uuid: spot.uuid
+                  })
+                }
+              />
+            ))}
+          </Swiper>
+        </Absolute>
+      </Relative>
+    )
+  }
+}
+
+SpotsMap.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func.isRequired
+  }).isRequired,
+  // TODO: use fragment instead!
+  spots: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      address: PropTypes.shape({
+        lat: PropTypes.number.isRequired,
+        lng: PropTypes.number.isRequired
+      }).isRequired
+    }).isRequired
+  ),
+  delta: PropTypes.shape({
+    latitudeDelta: PropTypes.number.isRequired,
+    longitudeDelta: PropTypes.number.isRequired
+  }).isRequired
+}
+
+SpotsMap.defaultProps = {
+  spots: [],
+  delta: {
+    latitudeDelta: LATITUDE_DELTA,
+    longitudeDelta: LONGITUDE_DELTA
+  }
+}
+
+export default SpotsMap */
--- App/Components/Spots/SpotMap.js
+++ App/Components/Spots/SpotMap.js
@@ -1,35 +1,18 @@
 import React from 'react'
-import { ScrollView, View, TouchableOpacity } from 'react-native'
+import { ScrollView, View } from 'react-native' // eslint-disable-line
 import PropTypes from 'prop-types'
 import { Query } from 'react-apollo'
-import styled from 'styled-components'
 import { GET_SPOTS } from './SpotList'
-import Colors from '../../Themes/Colors'
 import Text from '../Text'
 import SpotsMap from '../Maps/SpotsMap'
 
 // TODO: handle no spots were found case
-
-const Container = styled(ScrollView)`
-  background-color: ${Colors.white};
-`
-
-const ImageSwiperContainer = styled.View`
-  height: 200px;
-`
-
-const HorizontalView = styled.View`
-  flex-direction: row;
-`
 
 const SpotMap = ({ navigation }) => (
   <Query query={GET_SPOTS}>
     {({ loading, error, data }) => {
       if (loading) return <Text>Loading...</Text>
       if (error) return <Text>Error :( {JSON.stringify(error)}</Text>
-
-      console.log('\nSPOTS', data.spots)
-
       return <SpotsMap navigation={navigation} spots={data.spots} />
     }}
   </Query>
@@ -42,21 +25,3 @@ SpotMap.propTypes = {
 }
 
 export default SpotMap
-
-/*
-          <FlatList
-            showsVerticalScrollIndicator={false}
-            data={data.spots.slice(0, 100)}
-            renderItem={({ item }) => (
-              <CardContainer
-                spot={item}
-                onPress={() =>
-                  navigation.navigate('SpotDetailsScreen', {
-                    uuid: item.uuid
-                  })
-                }
-              />
-            )}
-            keyExtractor={item => item.uuid}
-          />
-*/
