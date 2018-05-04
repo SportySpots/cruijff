a5e1446ffc545bacbe28e39a82a45a3c9a343913
Tom Klaver <tomklav@gmail.com>
2018-05-04 19:32:33 +0200

    test

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
-
-if [ "$APPCENTER_BRANCH" != "master" ];
-then
-    export
-    pwd
-    ls
-    echo "GOOGLE_MAPS_API_KEY=${GOOGLE_MAPS_API_KEY}" > $APPCENTER_SOURCE_DIRECTORY/.env
+export
+pwd
+ls
+if [ "$APPCENTER_BRANCH" != "master" ]; then
+    echo "GOOGLE_MAPS_API_KEY=${GOOGLE_MAPS_API_KEY}" > $APPCENTER_SOURCE_DIRECTORY/.env;
 fi
