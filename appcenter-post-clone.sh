eb19134b2940c9ce06b30e0f0d88ab36ffff9b52
Tom Klaver <tomklav@gmail.com>
2018-05-04 19:44:33 +0200

    test again

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+echo "GOOGLE_MAPS_API_KEY=${GOOGLE_MAPS_API_KEY}" > ${APPCENTER_SOURCE_DIRECTORY}/.env;
 export
 pwd
 ls
+cat .env
--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
-export
-pwd
-ls
-if [ "$APPCENTER_BRANCH" != "master" ]; then
-    echo "GOOGLE_MAPS_API_KEY=${GOOGLE_MAPS_API_KEY}" > $APPCENTER_SOURCE_DIRECTORY/.env;
-fi
+
+echo "PRE_BUILD"
