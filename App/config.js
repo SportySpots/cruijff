41bb0a27ad521fe425068635cf27d020f64d59cd
Tom Klaver <tomklav@gmail.com>
2018-05-09 17:56:02 +0200

    setup react native config

--- App/config.js
+++ App/config.js
@@ -1,5 +1,6 @@
 import { Text } from 'react-native'
 import './I18n/I18n'
+import Config from 'react-native-config'
 
 if (typeof global.self === 'undefined') {
   // needed for apollo client
@@ -10,35 +11,17 @@ if (typeof global.self === 'undefined') {
 Text.defaultProps.allowFontScaling = true
 
 const settings = {
-  seedorfRestUrl: 'https://sportyspots.com/api',
-  seedorfGraphQLUrl: 'https://sportyspots.com/graphql',
-  useFixtures: false
+  seedorfRestUrl: Config.SEEDORF_REST_URL,
+  seedorfGraphQLUrl: Config.SEEDORF_GRAPHQL_URL,
+  useFixtures: Config.USE_FIXTURES === 'YES'
 }
 
 if (__DEV__) {
   // If ReactNative's yellow box warnings are too much, it is possible to turn
   // it off, but the healthier approach is to fix the warnings.  =)
   console.disableYellowBox = false
-
-  // https://stackoverflow.com/questions/39022216/react-native-programmatically-check-if-in-remote-js-debugging-is-enabled
-  const isDebuggingEnabled = typeof atob !== 'undefined'
-
-  /* When using React Native Debugger with Network debug enabled, all network calls are proxied from the host system.
-     So in this case use localhost, otherwise the host IP as seen from the device 10.0.3.2.
-     Genymotion - host @ 10.0.3.2
-     Android studio emulator - host @ 10.0.2.2
-   */
-  const networkDebugEnabled = false // set to false if not using network debugger
-  settings.seedorfRestUrl =
-    isDebuggingEnabled && networkDebugEnabled
-      ? 'http://localhost:8000/api'
-      : 'http://10.0.2.2:8000/api'
-  settings.seedorfGraphQLUrl =
-    isDebuggingEnabled && networkDebugEnabled
-      ? 'http://localhost:8000/graphql'
-      : 'http://10.0.2.2:8000/graphql'
-
-  settings.useFixtures = false
 }
+
+console.log(settings)
 
 export default settings
--- appcenter-post-clone.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-echo "GOOGLE_MAPS_API_KEY=${GOOGLE_MAPS_API_KEY}" > ${APPCENTER_SOURCE_DIRECTORY}/.env;
-export
-pwd
-ls
-cat .env
