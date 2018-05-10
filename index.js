9a5b1b89b0e3629f24126cd5d7ef232718b6abd3
Tom Klaver <tomklav@gmail.com>
2018-05-10 16:55:14 +0200

    add Object.setPrototypeOf polyfill

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 import { AppRegistry } from 'react-native'
 import App from './App/App'
 
+// https://github.com/apollographql/apollo-client/issues/3236
+Object.setPrototypeOf = Object.setPrototypeOf || function (obj, proto) {
+  obj.__proto__ = proto
+  return obj
+}
+
 AppRegistry.registerComponent('SportySpots', () => App)
