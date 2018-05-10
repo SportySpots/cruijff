21f1f03dc23e92082f454637aa530a3a8e04c60c
ashutosh <ashutoshb@gmail.com>
2018-05-10 10:29:47 +0200

    debug post clone script

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -5,8 +5,12 @@ brew install git-crypt
 
 cd $APPCENTER_SOURCE_DIRECTORY
 
+echo $GIT_CRYPT_BASE64_SYMMETRIC_KEY
+
 # decode base64 encoded symmetric key
 echo $GIT_CRYPT_BASE64_SYMMETRIC_KEY | openssl base64 -d -out git_crypt_symmetric_base64_decoded.key
 
 # unlock git crypt encrypted files
 git-crypt unlock git_crypt_symmetric_base64_decoded.key
+
+cat .env.prd
