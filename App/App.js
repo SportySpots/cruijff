0ee00eb7d3d09fb73b2b391e38b5d827f344e785
ashutosh <ashutoshb@gmail.com>
2018-05-23 01:05:04 +0200

    add onesignal, update pods, add push notifications

--- App/App.js
+++ App/App.js
@@ -9,7 +9,7 @@ import createStore from './Redux';
 import Colors from './Themes/Colors';
 import config from './config';
 import codePush from "react-native-code-push";
-
+import OneSignal from 'react-native-onesignal';
 
 class App extends Component {
   constructor() {
@@ -21,6 +21,35 @@ class App extends Component {
   // NOTE: https://github.com/Microsoft/react-native-code-push/issues/516#issuecomment-275688344
   // To remove warning caused by required listener
   codePushDownloadDidProgress(progress) {
+  }
+
+  componentWillMount() {
+    OneSignal.init('YOUR_ONESIGNAL_APPID');
+
+    OneSignal.addEventListener('received', this.onReceived);
+    OneSignal.addEventListener('opened', this.onOpened);
+    OneSignal.addEventListener('ids', this.onIds);
+  }
+
+  componentWillUnmount() {
+    OneSignal.removeEventListener('received', this.onReceived);
+    OneSignal.removeEventListener('opened', this.onOpened);
+    OneSignal.removeEventListener('ids', this.onIds);
+  }
+
+  onReceived(notification) {
+    console.log("Notification received: ", notification);
+  }
+
+  onOpened(openResult) {
+    console.log('Message: ', openResult.notification.payload.body);
+    console.log('Data: ', openResult.notification.payload.additionalData);
+    console.log('isActive: ', openResult.notification.isAppInFocus);
+    console.log('openResult: ', openResult);
+  }
+
+  onIds(device) {
+    console.log('Device info: ', device);
   }
 
   render() {
