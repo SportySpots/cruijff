d9d1e006bb4c096e18a3dea9458e2d725e7f4401
ashutosh <ashutoshb@gmail.com>
2018-05-23 22:59:03 +0200

    fix react native maps for ios

--- App/App.js
+++ App/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { ApolloProvider } from 'react-apollo';
 import { StatusBar } from 'react-native';
+import codePush from "react-native-code-push";
 import { Provider } from 'react-redux';
 import styled from 'styled-components';
 import { createClient, createMockClient } from './GraphQL/index';
@@ -8,8 +9,6 @@ import AppNavigation from './Navigation/AppNavigation';
 import createStore from './Redux';
 import Colors from './Themes/Colors';
 import config from './config';
-import codePush from "react-native-code-push";
-import OneSignal from 'react-native-onesignal';
 
 class App extends Component {
   constructor() {
@@ -21,36 +20,6 @@ class App extends Component {
   // NOTE: https://github.com/Microsoft/react-native-code-push/issues/516#issuecomment-275688344
   // To remove warning caused by required listener
   codePushDownloadDidProgress(progress) {
-  }
-
-  componentWillMount() {
-    console.log(OneSignal);
-    OneSignal.init('YOUR_ONESIGNAL_APPID');
-
-    OneSignal.addEventListener('received', this.onReceived);
-    OneSignal.addEventListener('opened', this.onOpened);
-    OneSignal.addEventListener('ids', this.onIds);
-  }
-
-  componentWillUnmount() {
-    OneSignal.removeEventListener('received', this.onReceived);
-    OneSignal.removeEventListener('opened', this.onOpened);
-    OneSignal.removeEventListener('ids', this.onIds);
-  }
-
-  onReceived(notification) {
-    console.log("Notification received: ", notification);
-  }
-
-  onOpened(openResult) {
-    console.log('Message: ', openResult.notification.payload.body);
-    console.log('Data: ', openResult.notification.payload.additionalData);
-    console.log('isActive: ', openResult.notification.isAppInFocus);
-    console.log('openResult: ', openResult);
-  }
-
-  onIds(device) {
-    console.log('Device info: ', device);
   }
 
   render() {
