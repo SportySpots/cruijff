949e2da41b490dbb540af33cb42960999cfad993
Tom Klaver <tomklav@gmail.com>
2018-05-24 15:40:43 +0200

    remove react-native-maps stuff from android build

--- android/app/src/main/java/com/sportyspots/android/MainApplication.java
+++ android/app/src/main/java/com/sportyspots/android/MainApplication.java
@@ -19,7 +19,7 @@ import com.microsoft.appcenter.reactnative.analytics.AppCenterReactNativeAnalyti
 import com.microsoft.appcenter.reactnative.appcenter.AppCenterReactNativePackage;
 import com.microsoft.appcenter.reactnative.crashes.AppCenterReactNativeCrashesPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
-import com.airbnb.android.react.maps.MapsPackage;
+//import com.airbnb.android.react.maps.MapsPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -53,8 +53,8 @@ public class MainApplication extends Application implements ReactApplication {
             new AppCenterReactNativePackage(MainApplication.this),
             new ReactNativeI18n(),
             new VectorIconsPackage(),
-            new RNDeviceInfo(),
-            new MapsPackage()
+            new RNDeviceInfo()
+//            new MapsPackage()
       );
     }
 
