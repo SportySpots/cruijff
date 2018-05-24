2cd4337e1948f3177bed94b9d3d478b36a2ca85a
Tom Klaver <tomklav@gmail.com>
2018-05-24 13:49:09 +0200

    add more uuids

--- App/Screens/Spots/SpotDetailsScreen.js
+++ App/Screens/Spots/SpotDetailsScreen.js
@@ -23,10 +23,13 @@ export const GET_SPOT_DETAILS = gql`
       uuid
       name
       images {
+        uuid
         image
       }
       amenities {
+        uuid
         sport {
+          uuid
           category
         }
         data
@@ -36,6 +39,7 @@ export const GET_SPOT_DETAILS = gql`
         category
       }
       address {
+        uuid
         lat
         lng
       }
