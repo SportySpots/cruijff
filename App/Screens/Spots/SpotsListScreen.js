69da320d0a8aaecf4455c522e7ad6f7d0056c5a1
fede-rodes <rodesf@gmail.com>
2018-05-27 18:58:56 +0200

    cleaning

--- App/Screens/Spots/SpotsListScreen.js
+++ App/Screens/Spots/SpotsListScreen.js
@@ -52,39 +52,3 @@ SpotsListScreen.propTypes = {
 };
 
 export default SpotsListScreen;
-
-/*
-import React from 'react';
-import PropTypes from 'prop-types';
-import SpotsList from '../../Components/Spots/SpotsList';
-import withQuery from '../../GraphQL/withQuery';
-import spotsQuery from '../../GraphQL/Spots/Queries/spots';
-import Card from '../../Components/Spots/SpotListCard';
-
-class SpotsListScreen extends React.Component {
-  static propTypes = {
-    navigation: PropTypes.shape({
-      navigate: PropTypes.func.isRequired,
-    }).isRequired,
-  }
-
-  handleCardPress = (spotId) => {
-    this.props.navigation.navigate('SpotDetailsScreen', {
-      uuid: spotId,
-    });
-  };
-
-  render() {
-    const Contents = withQuery(spotsQuery)(SpotsList);
-    return (
-      <Contents
-        cardComponent={Card}
-        onCardPress={uuid => this.handleCardPress(uuid)}
-        style={this.props.style}
-      />
-    );
-  }
-}
-
-export default SpotsListScreen;
-*/
