0c1e93a8f280f2559c73545191ed9156c366c2e4
fede-rodes <rodesf@gmail.com>
2018-06-03 15:15:53 +0200

    moving GET_GAME_DETAILS

---
+++ App/GraphQL/Games/Fragments/gameDetails.js
@@ -0,0 +1,59 @@
+import gql from 'graphql-tag';
+
+const gameDetailsFragment = gql`
+  fragment gameDetailsFragment on GameType {
+    uuid
+    name
+    start_time
+    end_time
+    is_featured
+    show_remaining
+    capacity
+    description
+    sport {
+      uuid
+      category
+    }
+    spot {
+      uuid
+      name
+      images {
+        uuid
+        image
+      }
+      amenities {
+        uuid
+        sport {
+          uuid
+          category
+        }
+        data
+      }
+      sports {
+        uuid
+        category
+      }
+      address {
+        uuid
+        lat
+        lng
+      }
+    }
+    #organizer {
+    #  uuid
+    #  first_name
+    #  last_name
+    #}
+    attendees {
+      uuid
+      status
+      #user {
+      #  uuid
+      #  first_name
+      #  last_name
+      #}
+    }
+  }
+`;
+
+export default gameDetailsFragment;
