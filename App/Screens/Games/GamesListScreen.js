7ffc9567d2309310de39903ab4fa320d5701fee1
fede-rodes <rodesf@gmail.com>
2018-06-03 16:08:58 +0200

    adding comment to curatedGames aux function

--- App/Screens/Games/GamesListScreen.js
+++ App/Screens/Games/GamesListScreen.js
@@ -20,6 +20,11 @@ const Container = styled(View)`
 //------------------------------------------------------------------------------
 // AUX FUNCTIONS:
 //------------------------------------------------------------------------------
+/**
+ * @summary (This is a hack) for some reason some of the games have no spot
+ * associated to them, so we filter them in order to avoid errors in the child
+ * components.
+ */
 const curatedGames = games => (
   games && games.length > 0 ? games.filter(game => game.spot) : []
 );
@@ -94,118 +99,6 @@ GamesListScreen.propTypes = {
 };
 
 export default GamesListScreen;
-
-/*
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Query } from 'react-apollo';
-import { View } from 'react-native';
-import styled from 'styled-components';
-import Colors from '../../Themes/Colors';
-import GET_GAMES_LIST from '../../GraphQL/Games/Queries/GET_GAMES_LIST';
-import Text from '../../Components/Text';
-import GamesList from '../../Components/Games/GamesList';
-import Card from '../../Components/Games/GameListCard';
-
-//------------------------------------------------------------------------------
-// STYLE:
-//------------------------------------------------------------------------------
-const Container = styled(View)`
-  flex: 1;
-  padding: 0 8px;
-  background-color: ${Colors.white};
-`;
-//------------------------------------------------------------------------------
-// AUX FUNCTIONS:
-//------------------------------------------------------------------------------
-const curatedGames = games => (
-  games && games.length > 0 ? games.filter(game => game.spot) : []
-);
-//------------------------------------------------------------------------------
-// COMPONENT:
-//------------------------------------------------------------------------------
-class GamesListScreen extends React.Component {
-  handleCardPress = (gameId) => {
-    this.props.navigation.navigate('GameDetailsScreen', {
-      uuid: gameId,
-    });
-  }
-
-  render() {
-    return (
-      <Query
-        query={GET_GAMES_LIST}
-        variables={{
-          offset: 0,
-          limit: 20,
-          ordering: 'start_time',
-        }}
-        fetchPolicy="cache-and-network"
-      >
-        {({
-          loading,
-          error,
-          data,
-          refetch,
-          fetchMore,
-        }) => {
-          if (error) return <Text>Error :( {JSON.stringify(error)}</Text>;
-
-          console.log(
-            '*****data.games.length', data && data.games && data.games.length || 0,
-            '*****data.games.length', data && data.games && curatedGames(data.games).length || 0,
-          );
-
-          const loadMore = () => {
-            fetchMore({
-              variables: {
-                offset: data.games.length,
-              },
-              updateQuery: (prev, { fetchMoreResult }) => {
-                console.log(
-                  '*****prev.games.length', curatedGames(prev.games).length,
-                  '*****fetchMoreResult.games.length', curatedGames(fetchMoreResult.games).length,
-                );
-                // Don't do anything if there weren't any new items
-                // if (!fetchMoreResult) return prev;
-                if (!fetchMoreResult) return Object.assign({}, { games: curatedGames(prev.games) });
-                // Concatenate the new feed results after the old ones
-                /* return Object.assign({}, prev, {
-                  games: [...curatedGames(prev.games), ...curatedGames(fetchMoreResult.games)],
-                }); //
-                return Object.assign({}, { games: curatedGames(fetchMoreResult.games) });
-              },
-            });
-          };
-
-          return (
-            <Container>
-              <GamesList
-                games={(data && data.games && curatedGames(data.games)) || []}
-                cardComponent={Card}
-                onCardPress={this.handleCardPress}
-                // FlatList props
-                onRefresh={refetch}
-                refreshing={loading}
-                onEndReached={loadMore}
-                onEndReachedThreshold={0.5}
-              />
-            </Container>
-          );
-        }}
-      </Query>
-    );
-  }
-}
-
-GamesListScreen.propTypes = {
-  navigation: PropTypes.shape({
-    navigate: PropTypes.func.isRequired,
-  }).isRequired,
-};
-
-export default GamesListScreen;
-*/
 
 /*
 import React, { Component } from 'react';
