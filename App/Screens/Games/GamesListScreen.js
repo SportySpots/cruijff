4b8b8383871d705580b07e8cf491f56644220c2b
fede-rodes <rodesf@gmail.com>
2018-06-07 09:57:34 +0200

    disabling pagination for games screen

--- App/Screens/Games/GamesListScreen.js
+++ App/Screens/Games/GamesListScreen.js
@@ -47,116 +47,10 @@ class GamesListScreen extends React.Component {
         query={GET_GAMES_LIST}
         variables={{
           offset: 0,
-          limit: 20,
-          ordering: 'start_time',
-        }}
-        fetchPolicy="cache-and-network"
-      >
-        {({
-          loading,
-          error,
-          data,
-          refetch,
-          fetchMore,
-        }) => {
-          if (error) return <Text>Error :( {JSON.stringify(error)}</Text>;
-
-          const loadMore = () => {
-            fetchMore({
-              variables: {
-                offset: (data && data.games && data.games.length) || 0,
-              },
-              updateQuery: (prev, { fetchMoreResult }) => {
-                if (!fetchMoreResult) return prev;
-                return Object.assign({}, prev, {
-                  games: [...prev.games, ...fetchMoreResult.games],
-                });
-              },
-            });
-          };
-
-          return (
-            <Container>
-              <GamesList
-                games={(data && data.games && curatedGames(data.games)) || []}
-                cardComponent={Card}
-                onCardPress={this.handleCardPress}
-                // FlatList props
-                onRefresh={() => { if (!loading) { refetch(); } }}
-                refreshing={loading}
-                onEndReached={() => { if (!loading) { loadMore(); } }}
-                onEndReachedThreshold={0.1}
-              />
-            </Container>
-          );
-        }}
-      </Query>
-    );
-  }
-}
-
-GamesListScreen.propTypes = {
-  navigation: PropTypes.shape({
-    navigate: PropTypes.func.isRequired,
-  }).isRequired,
-};
-
-export default GamesListScreen;
-
-/*
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Query } from 'react-apollo';
-import { View } from 'react-native';
-import { uniqBy } from 'ramda';
-import styled from 'styled-components';
-import Colors from '../../Themes/Colors';
-import GET_GAMES_LIST from '../../GraphQL/Games/Queries/GET_GAMES_LIST';
-import Text from '../../Components/Text';
-import GamesList from '../../Components/Games/GamesList';
-import Card from '../../Components/Games/GameListCard';
-
-//------------------------------------------------------------------------------
-// STYLE:
-//------------------------------------------------------------------------------
-const Container = styled(View)`
-  flex: 1;
-  padding: 0 8px;
-  background-color: ${Colors.white};
-`;
-//------------------------------------------------------------------------------
-// AUX FUNCTIONS:
-//------------------------------------------------------------------------------
-/**
- * @summary (This is a hack) for some reason some of the games have no spot
- * associated to them, so we filter them in order to avoid errors in the child
- * components.
- //
-const curatedGames = games => (
-  games && games.length > 0
-    ? uniqBy(({ uuid }) => (uuid), games.filter(game => game.spot))
-    : []
-);
-//------------------------------------------------------------------------------
-// COMPONENT:
-//------------------------------------------------------------------------------
-class GamesListScreen extends React.Component {
-  handleCardPress = (gameId) => {
-    this.props.navigation.navigate('GameDetailsScreen', {
-      uuid: gameId,
-    });
-  }
-
-  render() {
-    return (
-      <Query
-        query={GET_GAMES_LIST}
-        variables={{
-          offset: 0,
           limit: 100,
           ordering: 'start_time',
         }}
-        // fetchPolicy="cache-and-network"
+        fetchPolicy="cache-and-network"
       >
         {({
           loading,
@@ -179,7 +73,7 @@ class GamesListScreen extends React.Component {
                 });
               },
             });
-          }; //
+          }; */
 
           return (
             <Container>
@@ -191,7 +85,7 @@ class GamesListScreen extends React.Component {
                 onRefresh={refetch}
                 refreshing={loading}
                 // onEndReached={loadMore}
-                onEndReachedThreshold={0}
+                // onEndReachedThreshold={0}
               />
             </Container>
           );
@@ -208,7 +102,6 @@ GamesListScreen.propTypes = {
 };
 
 export default GamesListScreen;
-*/
 
 /*
 import React, { Component } from 'react';
