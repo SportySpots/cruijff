ba9a650d9a512d45098499b43b20c593aa9fb0a1
Tom Klaver <tomklav@gmail.com>
2018-06-13 14:44:38 +0200

    add is_verified to all spots queries

--- App/GraphQL/Spots/Queries/GET_SPOTS.js
+++ App/GraphQL/Spots/Queries/GET_SPOTS.js
@@ -3,7 +3,7 @@ import spotFragment from '../Fragments/spot';
 
 const GET_SPOTS = gql`
   query spots($limit: Int, $offset: Int) {
-    spots(limit: $limit, offset: $offset) {
+    spots(limit: $limit, offset: $offset, is_verified: true) {
       ...spotFragment
     }
   }
@@ -12,7 +12,7 @@ const GET_SPOTS = gql`
 
 export const GET_SPOTS_FOR_SPORT = gql`
   query ($sport: String, $limit: Int, $offset: Int) {
-    spots(sports__category: $sport, limit: $limit, offset: $offset) {
+    spots(sports__category: $sport, limit: $limit, offset: $offset, is_verified: true) {
       ...spotFragment
     }
   }
