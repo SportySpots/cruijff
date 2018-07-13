e81a766fafc1334e632017369412153d9db186f1
fede-rodes <rodesf@gmail.com>
2018-07-13 11:56:22 +0200

    cleaning field component

--- App/Components/PlanGame/Field.js
+++ App/Components/PlanGame/Field.js
@@ -1,81 +1,51 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import gql from 'graphql-tag';
-import {
-  FlatList,
-  Modal,
-  StyleSheet,
-  TouchableOpacity,
-  View,
-} from 'react-native';
-import { Calendar } from 'react-native-calendars';
+import { TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import styled from 'styled-components';
 import Text from '../../Components/Text';
-import withQuery from '../../GraphQL/withQuery';
-import I18n from '../../I18n/index';
 import Colors from '../../Themes/Colors';
 
-
+//------------------------------------------------------------------------------
+// STYLE:
+//------------------------------------------------------------------------------
+const Container = styled.View`
+  flex-direction: row;
+  align-items: center;
+  margin-horizontal: 8px;
+`;
+//------------------------------------------------------------------------------
+const Block = styled.View`
+  border-bottom-width: 1px;
+  border-bottom-color: ${Colors.white};
+  padding-horizontal: 8px;
+`;
+//------------------------------------------------------------------------------
+const Value = styled(Text.M)`
+  font-size: 28px;
+`;
+//------------------------------------------------------------------------------
+// COMPONENT:
+//------------------------------------------------------------------------------
 const Field = ({ value, onPress }) => (
-  <TouchableOpacity onPress={() => onPress && onPress()}>
-    <View style={styles.fieldContainer}>
-      <View style={styles.fieldBlock}>
-        <Text.M style={styles.fieldValue}>{value}</Text.M>
-      </View>
+  <TouchableOpacity onPress={onPress}>
+    <Container>
+      <Block>
+        <Value>{value}</Value>
+      </Block>
       <Icon size={24} name="keyboard-arrow-down" />
-    </View>
+    </Container>
   </TouchableOpacity>
 );
 
-export default Field;
+Field.propTypes = {
+  value: PropTypes.string,
+  onPress: PropTypes.func,
+};
 
-const styles = StyleSheet.create({
-  outerContainer: {
-    flex: 1,
-    height: '100%',
-  },
-  container: {
-    backgroundColor: Colors.primaryGreen,
-    flex: 1,
-    paddingTop: 48,
-  },
-  horizontal: {
-    flexDirection: 'row',
-    marginVertical: 16,
-    alignItems: 'center',
-  },
-  title: {
-    color: Colors.white,
-    marginBottom: 32,
-    /* fontSize: 48, */
-  },
-  text: {
-    color: Colors.white,
-    fontSize: 24,
-  },
-  fieldContainer: {
-    marginHorizontal: 8,
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  fieldBlock: {
-    borderBottomWidth: 1,
-    borderBottomColor: Colors.white,
-    paddingHorizontal: 8,
-  },
-  fieldValue: {
-    fontSize: 28,
-  },
-  modalContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    backgroundColor: 'rgba(0,0,0,.8)',
-  },
-  modalInnerContainer: {
-    backgroundColor: Colors.white,
-    flex: 1,
-    margin: 36,
-    padding: 8,
-  },
-});
+Field.defaultProps = {
+  value: '',
+  onPress: () => {},
+};
+
+export default Field;
