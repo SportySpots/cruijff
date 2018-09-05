ef29f01ad5af62064266e5a644a00a7f5b9acd78
fede-rodes <rodesf@gmail.com>
2018-09-05 10:52:06 +0200

    adding images to images folder and cancel game confirmation modal

--- App/Components/Games/CancelGameConfirmationModal/index.js
+++ App/Components/Games/CancelGameConfirmationModal/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import I18n from '../../../I18n/index';
+import themeImages from '../../../Themes/Images';
 import Text from '../../../Components/Common/Text';
 import Modal from '../../../Components/Common/Modal';
 
@@ -26,11 +27,11 @@ const Centered = styled(Text.M)`
 // COMPONENT:
 //------------------------------------------------------------------------------
 const CancelGameConfirmationModal = ({ visible, onClose }) => (
-  <Modal
-    visible={visible}
-    onClose={onClose}
-  >
-    <Img source={{ uri: 'https://via.placeholder.com/150x150' }} />
+  <Modal visible={visible} onClose={onClose}>
+    <Img
+      source={themeImages.activityCancelledVisual}
+      style={{ width: 130, height: 130 }}
+    />
     <Spacer />
     <Centered>{`${I18n.t('The activity is cancelled')}.`}</Centered>
   </Modal>
@@ -47,77 +48,3 @@ CancelGameConfirmationModal.defaultProps = {
 };
 
 export default CancelGameConfirmationModal;
-
-/*
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Modal, View } from 'react-native';
-import styled from 'styled-components';
-import I18n from '../../../I18n/index';
-import Text from '../../../Components/Common/Text';
-import Colors from '../../../Themes/Colors';
-
-//------------------------------------------------------------------------------
-// STYLE:
-//------------------------------------------------------------------------------
-const StyledModal = styled(Modal)`
-  border: 1px solid green;
-`;
-//------------------------------------------------------------------------------
-const Overlay = styled.View`
-  flex: 1;
-  display: flex;
-  justify-content: center;
-  background-color: rgba(0, 0, 0, 0.8);
-`;
-//------------------------------------------------------------------------------
-const Flex = styled.View`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: ${Colors.white};
-  margin: 36px;
-  padding: 16px;
-  border-radius: 8px;
-`;
-//------------------------------------------------------------------------------
-const Img = styled.Image`
-  margin: 0 auto;
-  height: 150px;
-  width: 150px;
-  border-radius: 8px;
-`;
-//------------------------------------------------------------------------------
-// COMPONENT:
-//------------------------------------------------------------------------------
-const CancelGameConfirmationModal = ({ visible, onClose }) => (
-  <StyledModal
-    visible={visible}
-    animationType="fade"
-    onRequestClose={onClose}
-    transparent
-  >
-    <Overlay>
-      <Flex>
-        <View>
-          <Img source={{ uri: 'https://via.placeholder.com/150x150' }} />
-          <Text.L>{I18n.t('Select a date')}</Text.L>
-        </View>
-      </Flex>
-    </Overlay>
-  </StyledModal>
-);
-
-CancelGameConfirmationModal.propTypes = {
-  visible: PropTypes.bool,
-  onClose: PropTypes.func,
-};
-
-CancelGameConfirmationModal.defaultProps = {
-  visible: false,
-  onClose: () => {},
-};
-
-export default CancelGameConfirmationModal;
-
-*/
