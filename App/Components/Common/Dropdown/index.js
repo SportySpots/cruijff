409227b7bf7a0f2ad24c370a2b0c5a4c820836e5
fede-rodes <rodesf@gmail.com>
2018-09-17 15:17:22 +0200

    improvements on Dropdown styles

--- App/Components/Common/Dropdown/index.js
+++ App/Components/Common/Dropdown/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { View } from 'react-native';
 import { Dropdown as DropdownMUI } from 'react-native-material-dropdown';
 import styled from 'styled-components';
 import Icon from 'react-native-vector-icons/MaterialIcons';
@@ -30,23 +31,36 @@ const Dropdown = ({ theme, size, ...rest }) => {
           animationDuration={150}
           baseColor={isWhiteTheme ? Colors.white : Colors.black}
           lineWidth={1}
+          rippleOpacity={0}
           dropdownPosition={-3.5}
           // Hide default carret
           renderAccessory={() => (null)}
+          /* containerStyle={{
+            borderWidth: 1,
+            borderColor: 'red',
+            padding: 0,
+          }} */
           style={{
             fontSize: Fonts.style[size].fontSize,
-            lineHeight: Fonts.style[size].fontSize * 1.5,
+            lineHeight: Fonts.style[size].fontSize,
             fontFamily: Fonts.style[size].fontFamily,
             paddingHorizontal: 8,
+            /* borderWidth: 1,
+            borderColor: 'black', */
+            margin: 0,
+            padding: 0,
           }}
           {...rest}
         />
       </FullWidth>
-      <Icon
-        size={24}
-        name="keyboard-arrow-down"
-        color={isWhiteTheme ? Colors.white : Colors.black}
-      />
+      <View>
+        <Spacer orientation="column" size="L" />
+        <Icon
+          size={24}
+          name="keyboard-arrow-down"
+          color={isWhiteTheme ? Colors.white : Colors.black}
+        />
+      </View>
       <Spacer orientation="row" size="M" />
     </Row>
   );
