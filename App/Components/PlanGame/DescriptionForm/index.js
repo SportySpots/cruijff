73332ef037434d018d166f1d24cf9ef558886233
fede-rodes <rodesf@gmail.com>
2018-09-25 20:44:23 +0200

    adding PlamGame DescriptionForm comp

--- App/Components/PlanGame/DescriptionForm/index.js
+++ App/Components/PlanGame/DescriptionForm/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import PropTypes from 'prop-types';
+import DescriptionField from '../DescriptionField';
 
-const DescriptionForm = () => (
-  <View>
-    <Text>Spot Form</Text>
-  </View>
+//------------------------------------------------------------------------------
+// COMPONENT:
+//------------------------------------------------------------------------------
+const DescriptionForm = ({ description, onChange }) => (
+  <DescriptionField
+    value={description}
+    characterRestriction={120}
+    onChangeText={(value) => { onChange({ fieldName: 'sport', value }); }}
+    // size="ML"
+    theme="white"
+  />
 );
+
+DescriptionForm.propTypes = {
+  description: PropTypes.string.isRequired,
+  onChange: PropTypes.func,
+};
+
+DescriptionForm.defaultProps = {
+  onChange: () => {},
+};
 
 export default DescriptionForm;
