78d8351647a66b4ab72b01fe9385ddd723687fa8
fede-rodes <rodesf@gmail.com>
2018-10-01 11:40:22 +0200

    refactoring GamesList comp

--- App/Components/Games/GamesList/index.js
+++ App/Components/Games/GamesList/index.js
@@ -2,18 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { propType } from 'graphql-anywhere';
 import { FlatList, TouchableOpacity } from 'react-native';
-import styled from 'styled-components';
 import I18n from '../../../I18n';
 import gameFragment from '../../../GraphQL/Games/Fragments/game';
 import NothingFound from '../../Common/NothingFound';
+import Spacer from '../../Common/Spacer';
 
-//------------------------------------------------------------------------------
-// STYLE:
-//------------------------------------------------------------------------------
-// TODO: remove margin, use List spacer and Spacer comp
-const CardContainer = styled(TouchableOpacity)`
-  margin-vertical: 4px;
-`;
 //------------------------------------------------------------------------------
 // COMPONENT:
 //------------------------------------------------------------------------------
@@ -32,15 +25,18 @@ const GamesList = ({
       <NothingFound icon="calendar-plus" text={I18n.t('No games found')} />
     )}
     renderItem={({ item: game }) => (
-      <CardContainer
+      <TouchableOpacity
         key={game.uuid}
         onPress={() => { onCardPress(game.uuid); }}
         activeOpacity={1}
       >
         {React.createElement(cardComponent, { game })}
-      </CardContainer>
+      </TouchableOpacity>
     )}
     keyExtractor={item => item.uuid}
+    ItemSeparatorComponent={() => (
+      <Spacer orientation="column" size="M" />
+    )}
     {...rest}
   />
 );
