f78143feeacc30e4d9a1c72fe2b214d0751a3e57
fede-rodes <rodesf@gmail.com>
2018-10-09 17:34:08 +0200

    simplifying ShareGameButton

--- App/Components/Games/ShareGameButton/index.js
+++ App/Components/Games/ShareGameButton/index.js
@@ -1,22 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Share } from 'react-native';
+import config from '../../../config';
+import I18n from '../../../I18n';
 import RoundButton from '../../Common/RoundButton';
-import handleShare from './utils';
 
 //------------------------------------------------------------------------------
 // COMPONENT:
 //------------------------------------------------------------------------------
-const ShareGame = ({ gameUUID }) => (
-  <RoundButton
-    size="XL"
-    status="default"
-    iconName="link-variant"
-    onPress={() => { handleShare(gameUUID); }}
-  />
-);
+class ShareGameButton extends React.PureComponent {
+  handleShare = () => {
+    const { gameUUID } = this.props;
 
-ShareGame.propTypes = {
+    const url = `https://${config.deeplinkHost}/games/${gameUUID}`;
+    const message = `${I18n.t('You have been invited to a SportySpots game:')} ${url}`;
+
+    Share.share(
+      { message, title: 'SportySpots' },
+      { dialogTitle: I18n.t('share') },
+    );
+  }
+
+  render() {
+    return (
+      <RoundButton
+        size="XL"
+        status="default"
+        iconName="link-variant"
+        onPress={this.handleShare}
+      />
+    );
+  }
+}
+ShareGameButton.propTypes = {
   gameUUID: PropTypes.string.isRequired,
 };
 
-export default ShareGame;
+export default ShareGameButton;
