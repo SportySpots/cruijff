579cca401dae73a1c1ceb661326881204d9d42f4
fede-rodes <rodesf@gmail.com>
2018-10-28 15:16:31 +0100

    refactoring FieldBackground and SplashScreen

---
+++ App/Backgrounds/FieldBackground/index.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Dimensions, View } from 'react-native';
+import Svg, { Path } from 'react-native-svg';
+import styled from 'styled-components';
+import Colors from '../../Themes/Colors';
+import Logo from '../../Components/Common/Logo';
+import Spacer from '../../Components/Common/Spacer';
+
+//------------------------------------------------------------------------------
+// CONSTANTS:
+//------------------------------------------------------------------------------
+const { width: fullWidth, height: fullHeight } = Dimensions.get('window');
+//------------------------------------------------------------------------------
+// STYLE:
+//------------------------------------------------------------------------------
+const Container = styled.View`
+  flex: 1;
+`;
+//------------------------------------------------------------------------------
+const BgContainer = styled.View`
+  background-color: ${Colors.primaryGreen}
+  position: absolute;
+  left: 0;
+  top: 0;
+`;
+//------------------------------------------------------------------------------
+const FlexGrow = styled.View`
+  flex-grow: 1;
+`;
+//------------------------------------------------------------------------------
+// COMPONENT:
+//------------------------------------------------------------------------------
+const FieldBackground = ({ children }) => (
+  <Container>
+    <BgContainer>
+      <Svg width={fullWidth} height={fullHeight}>
+        <Path
+          d={`M${0.2 * fullWidth} 0 h ${0.08 * fullWidth} L ${0.16 *
+          fullWidth} ${fullHeight} h ${-0.12 * fullWidth} Z`}
+          fill="white"
+        />
+        <Path
+          d={`M0 0 h ${fullWidth} v ${0.5 * fullHeight} L 0 ${2 / 3 * fullHeight} Z`}
+          fill={Colors.secondaryDarkBlueGreen}
+          opacity=".84"
+        />
+      </Svg>
+    </BgContainer>
+    <Spacer size="XXXL" />
+    <Spacer size="XXXL" />
+    <View alignItems="center">
+      <Logo scale={1} />
+    </View>
+    <Spacer size="XXXL" />
+    <FlexGrow>
+      {children}
+    </FlexGrow>
+  </Container>
+);
+
+FieldBackground.propTypes = {
+  children: PropTypes.node,
+};
+
+FieldBackground.defaultProps = {
+  children: null,
+};
+
+export default FieldBackground;
--- App/Screens/SplashScreen.js
+++ App/Screens/SplashScreen.js
@@ -1,53 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { ActivityIndicator, AsyncStorage, TouchableOpacity } from 'react-native';
+import { AsyncStorage, TouchableOpacity } from 'react-native';
 import { connect } from 'react-redux';
 import styled from 'styled-components/native';
-import FieldBackground from '../Backgrounds/FieldBackground';
-import DefaultButton from '../Components/Common/DefaultButton';
-import Logo from '../Components/Common/Logo';
-import Text from '../Components/Common/Text';
 import I18n from '../I18n/index';
 import Colors from '../Themes/Colors';
+import FieldBackground from '../Backgrounds/FieldBackground';
+import RaisedButton from '../Components/Common/RaisedButton';
+import Block from '../Components/Common/Block';
+import Spacer from '../Components/Common/Spacer';
+import Text from '../Components/Common/Text';
 import globalRefs, { addGlobalRef } from '../globalRefs';
 
-const LogoContainer = styled.View`
-  flex: 1;
-  flex-direction: row;
-  align-items: flex-end;
-  justify-content: center;
-`;
-
-const TextContainer = styled.View`
-  flex: 1;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-`;
-
-const ButtonsContainer = styled.View`
-  flex: 1;
-  flex-direction: column;
-  justify-content: center;
-`;
-
-const SplashLabel = styled(Text.L)`
-  color: ${props => props.textColor || '#fff'}
+//------------------------------------------------------------------------------
+// STYLE:
+//------------------------------------------------------------------------------
+const Label = styled(Text.ML)`
+  color: ${Colors.white}
   text-align: center;
   font-size: 30px;
 `;
-
+//------------------------------------------------------------------------------
+const FlexOne = styled.View`
+  flex: 1;
+`;
+//------------------------------------------------------------------------------
 const LinkLabel = styled(Text.M)`
-  color: ${props => props.textColor || '#fff'}
+  color: ${Colors.white}
   text-align: center;
   text-decoration-line: underline;
 `;
-
-export class _SplashScreen extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { firstRun: null };
+//------------------------------------------------------------------------------
+// COMPONENT:
+//------------------------------------------------------------------------------
+class SplashScreen extends React.Component {
+  state = {
+    firstRun: null,
   }
+
   async componentDidMount() {
     globalRefs.SplashScreen = this;
     const firstRun = !await AsyncStorage.getItem('firstRunDone');
@@ -55,61 +45,73 @@ export class _SplashScreen extends React.Component {
     this.setState({ firstRun });
   }
 
+  componentWillReceiveProps(props) {
+    this.forwardIfLoggedIn(props);
+  }
+
   forwardIfLoggedIn = (props) => {
+    const { navigation } = this.props;
     if (props.user.uuid) {
-      this.props.navigation.navigate('MainNav');
+      navigation.navigate('MainNav');
     }
   };
 
-  componentWillReceiveProps(props) {
-    // this.forwardIfLoggedIn(props)
-  }
-
-  static propTypes = {
-    navigation: PropTypes.shape({
-      navigate: PropTypes.func.isRequired,
-    }).isRequired,
-  };
-
   render() {
-    const { navigate } = this.props.navigation;
+    const { navigation, user } = this.props;
+    const { firstRun } = this.state;
+
+    if (user && !user.initialized) {
+      return <FieldBackground />;
+    }
 
     return (
       <FieldBackground>
-        <LogoContainer>
-          <Logo />
-        </LogoContainer>
-        <TextContainer>
-          <SplashLabel testID="splashText">
-            {I18n.t('Discover sport locations and activities near you')}
-          </SplashLabel>
-        </TextContainer>
-        {this.props.user.initialized ? (
-          <ButtonsContainer>
-            <DefaultButton
-              testID="start"
-              onPress={() => navigate(this.state.firstRun ? 'OnboardingScreen' : 'MainNav')}
-              text={I18n.t('Start discovering')}
-              bgColor={Colors.actionYellow}
-              textColor="white"
-              activeOpacity={0.8}
-              accessibilityLabel="Start Discovering"
-            />
-            {!this.props.user.uuid && (
-              <TouchableOpacity testID="splashLoginButton" onPress={() => navigate('LoginScreen')}>
-                <LinkLabel>{I18n.t('Already signed up? Log in')}</LinkLabel>
-              </TouchableOpacity>
-            )}
-          </ButtonsContainer>
-        ) : (
-          <ButtonsContainer>
-            <ActivityIndicator size="large" color="#00ff00" />
-          </ButtonsContainer>
-        )}
+        <Label testID="splashText">
+          {I18n.t('Discover sport locations and activities near you')}
+        </Label>
+        <FlexOne />
+        <Block>
+          <RaisedButton
+            testID="start"
+            variant="default"
+            label={I18n.t('Start discovering')}
+            accessibilityLabel={I18n.t('Start discovering')}
+            onPress={() => {
+              navigation.navigate(firstRun ? 'OnboardingScreen' : 'MainNav');
+            }}
+          />
+          <Spacer size="XL" />
+          {(!user || !user.uuid) && (
+            <TouchableOpacity
+              testID="splashLoginButton"
+              onPress={() => { navigation.navigate('LoginScreen'); }}
+            >
+              <LinkLabel>{I18n.t('Already signed up? Log in')}</LinkLabel>
+            </TouchableOpacity>
+          )}
+        </Block>
+        <Spacer size="XL" />
       </FieldBackground>
     );
   }
 }
 
-const SplashScreen = connect(state => ({ user: state.user }))(_SplashScreen);
-export default SplashScreen;
+SplashScreen.propTypes = {
+  user: PropTypes.shape({
+    uuid: PropTypes.string,
+    initialized: PropTypes.bool,
+  }),
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
+SplashScreen.defaultProps = {
+  user: null,
+};
+
+// TODO: we need a user provider at top level to avoid using redux over and over again
+const mapStateToProps = ({ user }) => ({ user });
+const withRedux = connect(mapStateToProps, null);
+
+export default withRedux(SplashScreen);
