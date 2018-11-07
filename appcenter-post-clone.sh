a43b5a82628a1f5b0476b7450177632408c0b2e0
Tom Klaver <tomklav@gmail.com>
2018-11-07 14:05:31 +0100

    use node 8.12.0 in appcenter

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -2,7 +2,7 @@
 
 #update node
 brew uninstall node@6
-NODE_VERSION="8.9.4"
+NODE_VERSION="8.12.0"
 curl "https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}.pkg" > "$HOME/Downloads/node-installer.pkg"
 sudo installer -store -pkg "$HOME/Downloads/node-installer.pkg" -target "/"
 
