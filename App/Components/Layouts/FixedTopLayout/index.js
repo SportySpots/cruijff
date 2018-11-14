758e1f0b997853afa52d507a22d8128caf6d493d
fede-rodes <rodesf@gmail.com>
2018-11-14 18:00:46 +0100

    trying to add animation to SpotsFilterFlap

--- App/Components/Layouts/FixedTopLayout/index.js
+++ App/Components/Layouts/FixedTopLayout/index.js
@@ -18,3 +18,81 @@ export const BottomLayout = styled.ScrollView`
   flex: 1;
   background-color: ${Colors.concrete};
 `;
+
+/* import React from 'react';
+import PropTypes from 'prop-types';
+import { Animated } from 'react-native';
+import styled from 'styled-components';
+import Colors from '../../../Themes/Colors';
+
+//------------------------------------------------------------------------------
+// STYLE:
+//------------------------------------------------------------------------------
+const StyledTopLayout = styled.View`
+  display: flex;
+  justify-content: center;
+  height: 46px;
+  background-color: ${Colors.white}
+  border-bottom-width: 0.5px;
+  border-color: ${Colors.lightGray}
+  padding-horizontal: 16px;
+`;
+//------------------------------------------------------------------------------
+export const BottomLayout = styled.ScrollView`
+  flex: 1;
+  background-color: ${Colors.concrete};
+`;
+//------------------------------------------------------------------------------
+// COMPONENT:
+//------------------------------------------------------------------------------
+export class TopLayout extends React.PureComponent {
+  state = {
+    fadeAnim: new Animated.Value(0), // Initial value for opacity: 1
+  }
+
+  componentDidMount() {
+    const { fadeAnim } = this.state;
+
+    Animated.timing( // Animate over time
+      fadeAnim, // The animated value to drive
+      {
+        toValue: 1, // Animate to opacity: 1 (opaque)
+        duration: 1000, // Make it take a while
+      },
+    ).start(); // Starts the animation
+  }
+
+  componentWillUnmount() {
+    const { fadeAnim } = this.state;
+
+    Animated.timing( // Animate over time
+      fadeAnim, // The animated value to drive
+      {
+        toValue: 0, // Animate to opacity: 1 (opaque)
+        duration: 1000, // Make it take a while
+      },
+    ).start(); // Starts the animation
+  }
+
+  render() {
+    const { children } = this.props;
+    const { fadeAnim } = this.state;
+
+    return (
+      <Animated.View style={{ opacity: fadeAnim }}>
+        <StyledTopLayout>
+          {children}
+        </StyledTopLayout>
+      </Animated.View>
+    );
+  }
+}
+
+TopLayout.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.func,
+    PropTypes.node,
+  ]).isRequired,
+};
+
+*/
