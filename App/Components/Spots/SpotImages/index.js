b501c311626bb84c1bedd12cfc9647d3b423910c
federodes <federodes@gmail.com>
2018-11-23 13:57:57 +0100

    pulling spot images from imgix

--- App/Components/Spots/SpotImages/index.js
+++ App/Components/Spots/SpotImages/index.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Dimensions } from 'react-native';
 import styled from 'styled-components';
 import ImageSwiper from '../../Common/ImageSwiper';
 import { getSpotImages } from '../../../utils';
 
 //------------------------------------------------------------------------------
+// CONSTANTS:
+//------------------------------------------------------------------------------
+const HEIGHT = 200;
+const WIDTH = Dimensions.get('window').width;
+//------------------------------------------------------------------------------
 // STYLE:
 //------------------------------------------------------------------------------
 const Container = styled.View`
-  height: 200px;
+  height: ${HEIGHT}px;
 `;
 //------------------------------------------------------------------------------
 // COMPONENT:
 //------------------------------------------------------------------------------
 const SpotImages = ({ images }) => {
-  const imgs = getSpotImages(images);
+  const imgs = getSpotImages({ images, height: HEIGHT, width: WIDTH });
 
   return (
     <Container>
