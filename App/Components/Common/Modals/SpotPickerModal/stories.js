de5d1fdc94c0e57fee304b2a5b5509f223a3228e
federodes <federodes@gmail.com>
2018-11-30 14:14:29 +0100

    editGameScreen: adding spotPicker

---
+++ App/Components/Common/Modals/SpotPickerModal/stories.js
@@ -0,0 +1,27 @@
+import { storiesOf } from '@storybook/react-native';
+import React from 'react';
+import { View } from 'react-native';
+import ModalProps from '../../../../RenderProps/modal-props';
+import RaisedButton from '../../RaisedButton';
+import SpotPickerModal from './index';
+
+const Container = () => (
+  <ModalProps>
+    {({ visible, openModal, closeModal }) => (
+      <View>
+        <RaisedButton
+          label="Open Modal"
+          onPress={openModal}
+        />
+        <SpotPickerModal
+          visible={visible}
+          onClose={closeModal}
+          onSelect={closeModal}
+        />
+      </View>
+    )}
+  </ModalProps>
+);
+
+storiesOf('Modals.SpotPickerModal', module)
+  .add('SpotPickerModal', () => <Container />);
