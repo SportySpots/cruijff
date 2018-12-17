5c72e424eff99d4b6e282b4ab84dfbe7e184eeb7
federodes <federodes@gmail.com>
2018-12-17 11:14:36 +0100

    update PlayerRow text

--- App/Components/Games/PlayerRow/index.js
+++ App/Components/Games/PlayerRow/index.js
@@ -26,7 +26,7 @@ const PlayerRow = ({ player }) => {
         <View>
           <Text.M>{`${user.first_name} ${user.last_name}`}</Text.M>
           <Text.SM style={{ color: Colors.gray }}>
-            {I18n.t('playerRow.datePrefix')}
+            {I18n.t(player.status === 'ATTENDING' ? 'playerRow.signedUpOn' : 'playerRow.signedOutOn')}
             &nbsp;
             {moment.utc(createdAt).local().format('D MMMM HH:mm')}
           </Text.SM>
