ec02d257b9854493194fc057453d6590f668044c
federodes <federodes@gmail.com>
2019-01-30 13:45:23 +0100

    removing withUser from LoginEmailApiCall

--- App/Components/Auth/LoginEmailApiCall/index.js
+++ App/Components/Auth/LoginEmailApiCall/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 // import curateErrors from './utils';
-import { withUser, userPropTypes } from '../../../Context/User';
-import api from '../../../Services/SeedorfApi';
-import I18n from '../../../I18n';
+import SeedorfApi from '../../../Services/SeedorfApi';
 
 //------------------------------------------------------------------------------
 // COMPONENT:
@@ -14,20 +12,18 @@ import I18n from '../../../I18n';
  */
 class LoginEmailApiCall extends React.PureComponent {
   handleLogin = async (inputFields) => {
-    const { onLoginError, onLoginSuccess, login } = this.props;
+    const { onLoginError, onLoginSuccess } = this.props;
     const { email } = inputFields;
 
     try {
-      const response = await api.sendMagicLoginLink(email);
-      // const response = await login({ email });
-      // const response = undefined;
+      const response = await SeedorfApi.sendMagicLoginLink(email);
 
       // Pass event up to parent component
       if (response && response.problem) {
         // console.log('RESPONSE', response.data);
         // const errors = curateErrors(response.data);
         // onLoginError(errors);
-        onLoginError({ email: [I18n.t('loginEmailForm.fields.email.errors.notRegistered')] });
+        onLoginError({ email: ['loginEmailForm.fields.email.errors.notRegistered'] });
       } else {
         onLoginSuccess();
       }
@@ -55,7 +51,6 @@ LoginEmailApiCall.propTypes = {
   ]).isRequired,
   onLoginError: PropTypes.func,
   onLoginSuccess: PropTypes.func,
-  login: userPropTypes.login.isRequired,
 };
 
 LoginEmailApiCall.defaultProps = {
@@ -63,69 +58,4 @@ LoginEmailApiCall.defaultProps = {
   onLoginSuccess: () => {},
 };
 
-export default withUser(LoginEmailApiCall);
-
-/*
-import React from 'react';
-import PropTypes from 'prop-types';
-import curateErrors from './utils';
-import { withUser, userPropTypes } from '../../../Context/User';
-
-//------------------------------------------------------------------------------
-// COMPONENT:
-//------------------------------------------------------------------------------
-/**
- * @summary Gets input fields from the LoginEmailForm and calls API to
- * authenticate the user.
- /
-class LoginEmailApiCall extends React.PureComponent {
-  handleLogin = async (inputFields) => {
-    const { onLoginError, onLoginSuccess, login } = this.props;
-    const { email, password } = inputFields;
-
-    try {
-      const response = await login({ email, password });
-
-      // Pass event up to parent component
-      if (response && response.problem) {
-        console.log('RESPONSE', response.data);
-        const errors = curateErrors(response.data);
-        onLoginError(errors);
-      } else {
-        onLoginSuccess();
-      }
-    } catch (exc) {
-      onLoginError(exc);
-    }
-  }
-
-  render() {
-    const { children } = this.props;
-
-    // Public API
-    const api = {
-      loginUser: this.handleLogin,
-    };
-
-    return children(api);
-  }
-}
-
-LoginEmailApiCall.propTypes = {
-  children: PropTypes.oneOfType([
-    PropTypes.func,
-    PropTypes.object,
-  ]).isRequired,
-  onLoginError: PropTypes.func,
-  onLoginSuccess: PropTypes.func,
-  login: userPropTypes.login.isRequired,
-};
-
-LoginEmailApiCall.defaultProps = {
-  onLoginError: () => {},
-  onLoginSuccess: () => {},
-};
-
-export default withUser(LoginEmailApiCall);
-
-*/
+export default LoginEmailApiCall;
