438e1a5ba0a009d454b4df2e74f1387342bcddd0
federodes <federodes@gmail.com>
2019-02-06 12:13:13 +0100

    cleaning EmailLoginForm and adding tests

--- App/Components/Auth/LoginEmailForm/index.js
+++ App/Components/Auth/LoginEmailForm/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { View } from 'react-native';
+import cloneDeep from 'lodash/cloneDeep';
+import pick from 'lodash/pick';
 import ErrorHandling from 'error-handling-utils';
 import isEmail from 'validator/lib/isEmail';
 import I18n from '../../../I18n';
@@ -8,28 +10,33 @@ import Block from '../../Common/Block';
 import TextField from '../../Common/TextField';
 import RaisedButton from '../../Common/RaisedButton';
 
-// TODO: uset INIT_STATE, INIT_ERRORS
 //------------------------------------------------------------------------------
 // CONSTANTS:
 //------------------------------------------------------------------------------
 const MAX_CHARS = 120;
+
+const INIT_STATE = {
+  email: '',
+};
+
+const INIT_ERRORS = {
+  email: [],
+};
 //------------------------------------------------------------------------------
 // COMPONENT:
 //------------------------------------------------------------------------------
 class LoginEmailForm extends React.PureComponent {
   state = {
-    email: '',
-    errors: {
-      email: [],
-    },
+    ...cloneDeep(INIT_STATE),
+    errors: cloneDeep(INIT_ERRORS),
   }
 
   componentWillReceiveProps({ errors }) {
-    // Display server side errors coming from the outside
+    // Display (server side) errors coming from parent component
     if (errors) {
       this.setState({
         errors: {
-          email: [],
+          ...cloneDeep(INIT_ERRORS),
           ...errors,
         },
       });
@@ -37,11 +44,7 @@ class LoginEmailForm extends React.PureComponent {
   }
 
   clearErrors = () => {
-    this.setState({
-      errors: {
-        email: [],
-      },
-    });
+    this.setState({ errors: cloneDeep(INIT_ERRORS) });
   };
 
   handleChange = ({ fieldName, value }) => {
@@ -56,9 +59,7 @@ class LoginEmailForm extends React.PureComponent {
 
   validateFields = ({ email }) => {
     // Initialize errors
-    const errors = {
-      email: [],
-    };
+    const errors = cloneDeep(INIT_ERRORS);
 
     // Sanitize input
     const _email = email && email.trim(); // eslint-disable-line no-underscore-dangle
@@ -91,14 +92,11 @@ class LoginEmailForm extends React.PureComponent {
       return; // return silently
     }
 
-    // Get field values
-    const { email } = this.state;
-
     // Clear previous errors if any
     this.clearErrors();
 
     // Validate fields
-    const errors = this.validateFields({ email });
+    const errors = this.validateFields(this.state);
 
     // In case of errors, display on UI and return handler to parent component
     if (ErrorHandling.hasErrors(errors)) {
@@ -110,7 +108,7 @@ class LoginEmailForm extends React.PureComponent {
     }
 
     // Pass event up to parent component
-    onSuccessHook({ email });
+    onSuccessHook(pick(this.state, Object.keys(INIT_STATE)));
   }
 
   render() {
@@ -171,216 +169,3 @@ LoginEmailForm.defaultProps = {
 };
 
 export default LoginEmailForm;
-
-
-/*
-import React from 'react';
-import PropTypes from 'prop-types';
-// import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
-import ErrorHandling from 'error-handling-utils';
-import isEmail from 'validator/lib/isEmail';
-import styled from 'styled-components';
-import I18n from '../../../I18n';
-import Block from '../../Common/Block';
-import TextField from '../../Common/TextField';
-import RaisedButton from '../../Common/RaisedButton';
-
-//------------------------------------------------------------------------------
-// CONSTANTS:
-//------------------------------------------------------------------------------
-const MAX_CHARS = 120;
-//------------------------------------------------------------------------------
-// STYLE:
-//------------------------------------------------------------------------------
-const FlexOne = styled.ScrollView`
-  flex: 1; /* full height /
-  `;
-  //------------------------------------------------------------------------------
-  // COMPONENT:
-  //------------------------------------------------------------------------------
-  // TODO: use KeyboardAwareScrollView
-  class LoginEmailForm extends React.PureComponent {
-    state = {
-      email: '',
-      password: '',
-      errors: {
-        email: [],
-        password: [],
-      },
-    }
-
-    componentWillReceiveProps({ errors }) {
-      // Display server side errors coming from the outside
-      if (errors) {
-        this.setState({
-          errors: {
-            email: [],
-            password: [],
-            ...errors,
-          },
-        });
-      }
-    }
-
-    clearErrors = () => {
-      this.setState({
-        errors: {
-          email: [],
-          password: [],
-        },
-      });
-    };
-
-    handleChange = ({ fieldName, value }) => {
-      const { errors } = this.state;
-
-      // Update value and clear errors for the given field
-      this.setState({
-        [fieldName]: value,
-        errors: ErrorHandling.clearErrors(errors, fieldName),
-      });
-    }
-
-    validateFields = ({ email, password }) => {
-      // Initialize errors
-      const errors = {
-        email: [],
-        password: [],
-      };
-
-      // Sanitize input
-      const _email = email && email.trim(); // eslint-disable-line no-underscore-dangle
-
-      if (!_email) {
-        errors.email.push('loginEmailForm.fields.email.errors.required');
-      } else if (!isEmail(_email)) {
-        errors.email.push('loginEmailForm.fields.email.errors.invalid');
-      } else if (_email.length > MAX_CHARS) {
-        errors.email.push('loginEmailForm.fields.email.errors.tooLong');
-      }
-
-      // Don't sanitize password
-      if (!password || password.length === 0) {
-        errors.password.push('loginEmailForm.fields.password.errors.required');
-      } else if (password.length > MAX_CHARS) {
-        errors.password.push('loginEmailForm.fields.password.errors.tooLong');
-      }
-
-      return errors;
-    };
-
-    handleSubmit = () => {
-      const {
-        onBeforeHook,
-        onClientCancelHook,
-        onClientErrorHook,
-        onSuccessHook,
-      } = this.props;
-
-      // Run before logic if provided and return on error. onBeforeHook will set the 'disabled'
-      // value to 'true' so that the user cannot re-submit the form
-      try {
-        onBeforeHook();
-      } catch (exc) {
-        onClientCancelHook();
-        return; // return silently
-      }
-
-      // Get field values
-      const { email, password } = this.state;
-
-      // Clear previous errors if any
-      this.clearErrors();
-
-      // Validate fields
-      const errors = this.validateFields({ email, password });
-
-      // In case of errors, display on UI and return handler to parent component
-      if (ErrorHandling.hasErrors(errors)) {
-        this.setState({ errors });
-        // Pass event up to parent component. onClientErrorHook will set 'disabled'
-        // value back to 'false' so that the user can re-submit the form
-        onClientErrorHook();
-        return;
-      }
-
-      // Pass event up to parent component
-      onSuccessHook({ email, password });
-    }
-
-    render() {
-      const { disabled } = this.props;
-      const { email, password, errors } = this.state;
-
-      // Apply translation and concatenate field errors (string)
-      const emailErrors = ErrorHandling.getFieldErrors(errors, 'email', I18n.t);
-      const passwordErrors = ErrorHandling.getFieldErrors(errors, 'password', I18n.t);
-
-      return [
-        <FlexOne key="top" testID="LoginScreen">
-          <Block midHeight>
-            <TextField
-              testID="loginInputEmail"
-              label={I18n.t('loginEmailForm.fields.email.label')}
-              placeholder="m.ali@boxinglegends.com"
-              value={email}
-              error={emailErrors}
-              size="ML"
-              disabled={disabled}
-              keyboardType="email-address"
-              autoFocus
-              onChangeText={(value) => {
-                this.handleChange({ fieldName: 'email', value });
-              }}
-            />
-          </Block>
-          <Block midHeight>
-            <TextField
-              testID="loginInputPassword"
-              label={I18n.t('loginEmailForm.fields.password.label')}
-              placeholder="••••••••"
-              value={password}
-              error={passwordErrors}
-              size="ML"
-              disabled={disabled}
-              secureTextEntry
-              onChangeText={(value) => {
-                this.handleChange({ fieldName: 'password', value });
-              }}
-            />
-          </Block>
-        </FlexOne>,
-        <Block key="bottom">
-          <RaisedButton
-            testID="loginSubmitButton"
-            variant="default"
-            label={I18n.t('loginEmailForm.btnLabel')}
-            disabled={disabled}
-            onPress={this.handleSubmit}
-          />
-        </Block>,
-      ];
-    }
-  }
-
-  LoginEmailForm.propTypes = {
-    disabled: PropTypes.bool,
-    errors: PropTypes.object, // eslint-disable-line
-    onBeforeHook: PropTypes.func,
-    onClientCancelHook: PropTypes.func,
-    onClientErrorHook: PropTypes.func,
-    onSuccessHook: PropTypes.func,
-  };
-
-  LoginEmailForm.defaultProps = {
-    disabled: false,
-    errors: null,
-    onBeforeHook: () => {},
-    onClientCancelHook: () => {},
-    onClientErrorHook: () => {},
-    onSuccessHook: () => {},
-  };
-
-  export default LoginEmailForm;
-
-*/
---
+++ App/Components/Auth/LoginEmailForm/test.js
@@ -0,0 +1,116 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
+import I18n from '../../../I18n';
+import LoginEmailForm from '.';
+
+describe('LoginEmailForm', () => {
+  it('renders without crashing', () => {
+    const rendered = renderer.create(<LoginEmailForm />).toJSON();
+    expect(rendered).toBeTruthy();
+  });
+
+  it('renders custom label button', () => {
+    const wrapper = shallow(<LoginEmailForm />);
+    expect(wrapper.find({ testID: 'loginSubmitButton' }).props().label).toBe(I18n.t('loginEmailForm.btnLabel'));
+  });
+
+  it('errors when form is submitted without email', () => {
+    const handleClientError = jest.fn();
+    const wrapper = shallow(<LoginEmailForm onClientErrorHook={handleClientError} />);
+
+    // Sanity check
+    expect(wrapper.find({ testID: 'loginInputEmail' }).props().value).toBe('');
+
+    wrapper.find({ testID: 'loginSubmitButton' }).props().onPress();
+
+    expect(wrapper.find({ testID: 'loginInputEmail' }).props().error).toBe(I18n.t('loginEmailForm.fields.email.errors.required'));
+    expect(handleClientError).toBeCalled();
+  });
+
+  it('errors when form is submitted with invalid email', () => {
+    const handleClientError = jest.fn();
+    const wrapper = shallow(<LoginEmailForm onClientErrorHook={handleClientError} />);
+
+    // Sanity check
+    expect(wrapper.find({ testID: 'loginInputEmail' }).props().value).toBe('');
+
+    wrapper.find({ testID: 'loginInputEmail' }).props().onChangeText('invalid@email');
+    wrapper.find({ testID: 'loginSubmitButton' }).props().onPress();
+
+    expect(wrapper.find({ testID: 'loginInputEmail' }).props().error).toBe(I18n.t('loginEmailForm.fields.email.errors.invalid'));
+    expect(handleClientError).toBeCalled();
+  });
+
+  it('clears errors when email input field is modified after error', () => {
+    const handleClientError = jest.fn();
+    const wrapper = shallow(<LoginEmailForm onClientErrorHook={handleClientError} />);
+
+    // Sanity check
+    expect(wrapper.find({ testID: 'loginInputEmail' }).props().value).toBe('');
+
+    wrapper.find({ testID: 'loginInputEmail' }).props().onChangeText('invalid@email');
+    wrapper.find({ testID: 'loginSubmitButton' }).props().onPress();
+
+    expect(wrapper.find({ testID: 'loginInputEmail' }).props().error).toBe(I18n.t('loginEmailForm.fields.email.errors.invalid'));
+    expect(handleClientError).toBeCalled();
+
+    wrapper.find({ testID: 'loginInputEmail' }).props().onChangeText('valid@email.com');
+    wrapper.find({ testID: 'loginSubmitButton' }).props().onPress();
+
+    expect(wrapper.find({ testID: 'loginInputEmail' }).props().error).toBe('');
+  });
+
+  it('aborts form submission if onBeforeHook throws', () => {
+    const handleBefore = jest.fn().mockImplementation(() => { throw new Error(); });
+    const handleClientCancel = jest.fn();
+    const handleSuccess = jest.fn();
+
+    const wrapper = shallow(
+      <LoginEmailForm
+        onBeforeHook={handleBefore}
+        onClientCancelHook={handleClientCancel}
+        onSuccessHook={handleSuccess}
+      />,
+    );
+
+    // Sanity check
+    expect(wrapper.find({ testID: 'loginInputEmail' }).props().value).toBe('');
+
+    wrapper.find({ testID: 'loginInputEmail' }).props().onChangeText('valid@email.com');
+    wrapper.find({ testID: 'loginSubmitButton' }).props().onPress();
+
+    expect(handleBefore).toBeCalled();
+    expect(handleClientCancel).toBeCalled();
+    expect(handleSuccess).not.toBeCalled();
+  });
+
+  it('calls onSuccessHook when valid email is provided', () => {
+    const handleBefore = jest.fn();
+    const handleClientCancel = jest.fn();
+    const handleClientError = jest.fn();
+    const handleSuccess = jest.fn();
+
+    const wrapper = shallow(
+      <LoginEmailForm
+        onBeforeHook={handleBefore}
+        onClientCancelHook={handleClientCancel}
+        onClientErrorHook={handleClientError}
+        onSuccessHook={handleSuccess}
+      />,
+    );
+
+    // Sanity check
+    expect(wrapper.find({ testID: 'loginInputEmail' }).props().value).toBe('');
+
+    wrapper.find({ testID: 'loginInputEmail' }).props().onChangeText('valid@email.com');
+
+    expect(wrapper.state().email).toBe('valid@email.com');
+
+    wrapper.find({ testID: 'loginSubmitButton' }).props().onPress();
+
+    expect(handleBefore).toBeCalled();
+    expect(handleClientCancel).not.toBeCalled();
+    expect(handleSuccess).toBeCalledWith(expect.objectContaining({ email: 'valid@email.com' }));
+  });
+});
