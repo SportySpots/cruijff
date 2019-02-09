0f78c8b6e3206e2afb2c73343690639a8da8a399
federodes <federodes@gmail.com>
2019-02-09 22:58:09 +0100

    cleaning CancelGameForm and adding tests

--- App/Components/Games/CancelGameForm/index.js
+++ App/Components/Games/CancelGameForm/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Alert } from 'react-native';
+import { Alert, View } from 'react-native';
 import { propType } from 'graphql-anywhere';
 import cloneDeep from 'lodash/cloneDeep';
 import pick from 'lodash/pick';
@@ -11,7 +11,7 @@ import { TopLayout, BottomLayout } from '../../Layouts/FixedBottomLayout';
 import Block from '../../Common/Block';
 import Divider from '../../Common/Divider';
 import Label from '../../Common/Label';
-import AlertMsg from '../../Common/AlertMsg';
+// import AlertMsg from '../../Common/AlertMsg';
 import RaisedButton from '../../Common/RaisedButton';
 import GameProperties from '../GameProperties';
 import ClickableAttendees from '../ClickableAttendees';
@@ -21,7 +21,7 @@ import { getAttendees } from '../utils';
 //------------------------------------------------------------------------------
 // CONSTANTS:
 //------------------------------------------------------------------------------
-const MAX_CHARS = 120;
+export const MAX_CHARS = 120;
 
 const INIT_STATE = {
   cancelMsg: '',
@@ -47,6 +47,7 @@ class CancelGameForm extends React.PureComponent {
 
   handleCancelMsgChange = (cancelMsg) => {
     const { errors } = this.state;
+    console.log('handleCancelMsgChange', cancelMsg);
 
     // Update value and clear errors for the given field
     this.setState({
@@ -140,257 +141,57 @@ class CancelGameForm extends React.PureComponent {
     // Apply translation and concatenate field errors (string)
     const cancelMsgErrors = ErrorHandling.getFieldErrors(errors, 'cancelMsg', I18n.t);
 
-    return [
-      <TopLayout key="top">
-        <Block>
-          <GameProperties game={game} />
-        </Block>
-        {attendees.length > 0 && [
-          <Divider key="divider-game-attendees" />,
-          <Block key="game-attendees">
-            <Label>{I18n.t('cancelGameForm.attending')}</Label>
-            <ClickableAttendees
-              attendees={attendees}
-              onAttendeesPress={onAttendeesPress}
-            />
-          </Block>,
-          <Divider key="divider-cancel-msg" />,
-          <Block key="cancel-msg">
-            <CancelMsg
-              value={cancelMsg}
-              disabled={disabled}
-              characterRestriction={MAX_CHARS}
-              onChangeText={this.handleCancelMsgChange}
-              error={cancelMsgErrors}
-            />
-          </Block>,
-          false && [
-            <Divider key="divider-alert-warning" />,
-            <Block key="alert-warning">
-              <AlertMsg
-                value={I18n.t('cancelGameForm.alertMsg')}
-                status="warning"
-              />
-            </Block>,
-          ],
-        ]}
-      </TopLayout>,
-      <BottomLayout key="bottom">
-        <RaisedButton
-          testID="cancelGameFormCancelButton"
-          variant="warning"
-          label={I18n.t('cancelGameForm.btnLabel')}
-          disabled={disabled}
-          onPress={this.handleSubmit}
-        />
-      </BottomLayout>,
-    ];
-  }
-}
-
-CancelGameForm.propTypes = {
-  game: propType(gameDetailsFragment).isRequired,
-  disabled: PropTypes.bool,
-  onBeforeHook: PropTypes.func,
-  onClientCancelHook: PropTypes.func,
-  onClientErrorHook: PropTypes.func,
-  onSuccessHook: PropTypes.func,
-  onAttendeesPress: PropTypes.func,
-};
-
-CancelGameForm.defaultProps = {
-  disabled: false,
-  onBeforeHook: () => {},
-  onClientCancelHook: () => {},
-  onClientErrorHook: () => {},
-  onSuccessHook: () => {},
-  onAttendeesPress: () => {},
-};
-
-export default CancelGameForm;
-
-
-/*
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Alert } from 'react-native';
-import { propType } from 'graphql-anywhere';
-import ErrorHandling from 'error-handling-utils';
-import I18n from '../../../I18n';
-import gameDetailsFragment from '../../../GraphQL/Games/Fragments/gameDetails';
-import { TopLayout, BottomLayout } from '../../Layouts/FixedBottomLayout';
-import Block from '../../Common/Block';
-import Divider from '../../Common/Divider';
-import Label from '../../Common/Label';
-import AlertMsg from '../../Common/AlertMsg';
-import RaisedButton from '../../Common/RaisedButton';
-import GameProperties from '../GameProperties';
-import ClickableAttendees from '../ClickableAttendees';
-import CancelMsg from '../CancelMsg';
-import { getAttendees } from '../utils';
-
-//------------------------------------------------------------------------------
-// CONSTANTS:
-//------------------------------------------------------------------------------
-const MAX_CHARS = 120;
-//------------------------------------------------------------------------------
-// COMPONENT:
-//------------------------------------------------------------------------------
-class CancelGameForm extends React.PureComponent {
-  state = {
-    cancelMsg: '',
-    errors: {
-      cancelMsg: [],
-    },
-  }
-
-  clearErrors = () => {
-    this.setState({ errors: { cancelMsg: [] } });
-  };
-
-  handleCancelMsgChange = (cancelMsg) => {
-    const { errors } = this.state;
-
-    // Update value and clear errors for the given field
-    this.setState({
-      cancelMsg,
-      errors: ErrorHandling.clearErrors(errors, 'cancelMsg'),
-    });
-  }
-
-  validateFields = ({ cancelMsg }) => {
-    // Initialize errors
-    const errors = {
-      cancelMsg: [],
-    };
-
-    // Sanitize input
-    const _cancelMsg = cancelMsg && cancelMsg.trim(); // eslint-disable-line no-underscore-dangle
-
-    if (_cancelMsg.length > MAX_CHARS) {
-      errors.cancelMsg.push('cancelGameForm.fields.cancelMsg.errors.tooLong');
-    }
-
-    return errors;
-  };
-
-  handleSubmit = () => {
-    const {
-      game,
-      onBeforeHook,
-      onClientCancelHook,
-      onClientErrorHook,
-      onSuccessHook,
-    } = this.props;
-
-    // Run before logic if provided and return on error. onBeforeHook will set the 'disabled'
-    // value to 'true' so that the user cannot re-submit the form
-    try {
-      onBeforeHook();
-    } catch (exc) {
-      onClientCancelHook();
-      return; // return silently
-    }
-
-    // Get field values
-    const { cancelMsg } = this.state;
-
-    // Clear previous errors if any
-    this.clearErrors();
-
-    // Validate fields
-    const errors = this.validateFields({ cancelMsg });
-
-    // In case of errors, display on UI and return handler to parent component
-    if (ErrorHandling.hasErrors(errors)) {
-      this.setState({ errors });
-      // Pass event up to parent component. onClientErrorHook will set 'disabled'
-      // value back to 'false' so that the user can re-submit the form
-      onClientErrorHook();
-      return;
-    }
-
-    // Display confirm alert
-    Alert.alert(
-      I18n.t('cancelGameForm.confirmAlert.header'),
-      I18n.t('cancelGameForm.confirmAlert.body'),
-      [
-        {
-          text: I18n.t('cancelGameForm.confirmAlert.footer.cancelBtnLabel'),
-          onPress: () => {
-            // Pass event up to parent component. onClientErrorHook will set 'disabled'
-            // value back to 'false' so that the user can re-submit the form
-            onClientCancelHook();
-          },
-          style: 'cancel',
-        },
-        {
-          text: I18n.t('cancelGameForm.confirmAlert.footer.okBtnLabel'),
-          onPress: () => {
-            // Pass event up to parent component. onSuccessHook 'disabled'
-            // value back to 'false' so that the user can re-submit the form
-            onSuccessHook({ gameUUID: game.uuid, cancelMsg });
-          },
-        },
-      ],
+    return (
+      <View style={{ flex: 1 }}>
+        <TopLayout>
+          <Block>
+            <GameProperties game={game} />
+          </Block>
+          {attendees.length > 0 && (
+            <View>
+              <Divider />
+              <Block>
+                <Label>{I18n.t('cancelGameForm.attending')}</Label>
+                <ClickableAttendees
+                  attendees={attendees}
+                  onAttendeesPress={onAttendeesPress}
+                />
+              </Block>
+              <Divider />
+              <Block>
+                <CancelMsg
+                  testID="cancelGameFormCancelMsgField"
+                  value={cancelMsg}
+                  disabled={disabled}
+                  characterRestriction={MAX_CHARS}
+                  onChangeText={this.handleCancelMsgChange}
+                  error={cancelMsgErrors}
+                />
+              </Block>
+              {/* false && [
+                <Divider key="divider-alert-warning" />,
+                <Block key="alert-warning">
+                  <AlertMsg
+                    value={I18n.t('cancelGameForm.alertMsg')}
+                    status="warning"
+                  />
+                </Block>,
+              ] */}
+            </View>
+          )}
+        </TopLayout>
+        <BottomLayout>
+          <RaisedButton
+            testID="cancelGameFormSubmitButton"
+            variant="warning"
+            label={I18n.t('cancelGameForm.btnLabel')}
+            disabled={disabled}
+            onPress={this.handleSubmit}
+          />
+        </BottomLayout>
+      </View>
     );
   }
-
-  render() {
-    const { game, disabled, onAttendeesPress } = this.props;
-    const { cancelMsg, errors } = this.state;
-
-    const attendees = getAttendees(game.attendees);
-
-    // Apply translation and concatenate field errors (string)
-    const cancelMsgErrors = ErrorHandling.getFieldErrors(errors, 'cancelMsg', I18n.t);
-
-    return [
-      <TopLayout key="top">
-        <Block>
-          <GameProperties game={game} />
-        </Block>
-        {attendees.length > 0 && [
-          <Divider key="divider-game-attendees" />,
-          <Block key="game-attendees">
-            <Label>{I18n.t('cancelGameForm.attending')}</Label>
-            <ClickableAttendees
-              attendees={attendees}
-              onAttendeesPress={onAttendeesPress}
-            />
-          </Block>,
-          <Divider key="divider-cancel-msg" />,
-          <Block key="cancel-msg">
-            <CancelMsg
-              value={cancelMsg}
-              disabled={disabled}
-              characterRestriction={MAX_CHARS}
-              onChangeText={this.handleCancelMsgChange}
-              error={cancelMsgErrors}
-            />
-          </Block>,
-          false && [
-            <Divider key="divider-alert-warning" />,
-            <Block key="alert-warning">
-              <AlertMsg
-                value={I18n.t('cancelGameForm.alertMsg')}
-                status="warning"
-              />
-            </Block>,
-          ],
-        ]}
-      </TopLayout>,
-      <BottomLayout key="bottom">
-        <RaisedButton
-          testID="cancelGameFormCancelButton"
-          variant="warning"
-          label={I18n.t('cancelGameForm.btnLabel')}
-          disabled={disabled}
-          onPress={this.handleSubmit}
-        />
-      </BottomLayout>,
-    ];
-  }
 }
 
 CancelGameForm.propTypes = {
@@ -413,5 +214,3 @@ CancelGameForm.defaultProps = {
 };
 
 export default CancelGameForm;
-
-*/
--- App/Components/Games/CancelGameForm/test.js
+++ App/Components/Games/CancelGameForm/test.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { Alert } from 'react-native';
+import renderer from 'react-test-renderer';
 import GET_GAME_DETAILS from '../../../GraphQL/Games/Queries/GET_GAME_DETAILS';
 import { createMockClient } from '../../../GraphQL';
-import CancelGameForm from '.';
+import I18n from '../../../I18n';
+import CancelGameForm, { MAX_CHARS } from '.';
+
+const validCancelMsg = new Array(MAX_CHARS + 1).join('a'); // aaaaaa... length = MAX_CHARS
+const invalidCancelMsg = new Array(MAX_CHARS + 2).join('a'); // aaaaaa... length = MAX_CHARS + 1
+
+jest.mock('Alert', () => ({ alert: jest.fn() }));
 
 describe('CancelGameForm', () => {
   let game;
@@ -14,6 +22,16 @@ describe('CancelGameForm', () => {
       variables: { uuid: 455 },
     });
     game = res.data.game; // eslint-disable-line prefer-destructuring
+  });
+
+  it('renders without crashing', () => {
+    const rendered = renderer.create(<CancelGameForm game={game} />).toJSON();
+    expect(rendered).toBeTruthy();
+  });
+
+  it('renders custom label button', () => {
+    const wrapper = shallow(<CancelGameForm game={game} />);
+    expect(wrapper.find({ testID: 'cancelGameFormSubmitButton' }).props().label).toBe(I18n.t('cancelGameForm.btnLabel'));
   });
 
   it('aborts form submission if onBeforeHook throws', () => {
@@ -30,15 +48,68 @@ describe('CancelGameForm', () => {
       />,
     );
 
-    expect(wrapper.find('RaisedButton')).toHaveLength(1);
-    wrapper.find('RaisedButton').first().props().onPress();
+    // Sanity check
+    expect(wrapper.find({ testID: 'cancelGameFormCancelMsgField' }).props().value).toBe('');
+
+    wrapper.find({ testID: 'cancelGameFormSubmitButton' }).props().onPress();
 
     expect(handleBefore).toBeCalled();
     expect(handleClientCancel).toBeCalled();
     expect(handleSuccess).not.toBeCalled();
   });
 
-  it('calls onErrorHook when cancelMsg.length > MAX_CHARS', () => {});
+  it('errors when cancelMsg longer than MAX_CHARS', () => {
+    const handleClientError = jest.fn();
+    const wrapper = shallow(
+      <CancelGameForm
+        game={game}
+        onClientErrorHook={handleClientError}
+      />,
+    );
 
-  it('calls onSuccessHook when no cancelMsg or cancelMsg.length <= MAX_CHARS is provided', () => {});
+    // Sanity check
+    expect(wrapper.find({ testID: 'cancelGameFormCancelMsgField' }).props().value).toBe('');
+
+    wrapper.find({ testID: 'cancelGameFormCancelMsgField' }).props().onChangeText(invalidCancelMsg);
+
+    wrapper.find({ testID: 'cancelGameFormSubmitButton' }).props().onPress();
+
+    expect(wrapper.find({ testID: 'cancelGameFormCancelMsgField' }).props().error).toBe(I18n.t('cancelGameForm.fields.cancelMsg.errors.tooLong'));
+    expect(handleClientError).toBeCalled();
+  });
+
+  // it('calls onSuccessHook when no cancelMsg or cancelMsg.length <= MAX_CHARS is provided', () => {
+  //   ['', validCancelMsg].forEach((cancelMsg) => {
+  //     const handleBefore = jest.fn();
+  //     const handleClientCancel = jest.fn();
+  //     const handleClientError = jest.fn();
+  //     const handleSuccess = jest.fn();
+
+  //     const wrapper = shallow(
+  //       <CancelGameForm
+  //         game={game}
+  //         onBeforeHook={handleBefore}
+  //         onClientCancelHook={handleClientCancel}
+  //         onClientErrorHook={handleClientError}
+  //         onSuccessHook={handleSuccess}
+  //       />,
+  //     );
+
+  //     // Sanity check
+  //     expect(wrapper.find({ testID: 'cancelGameFormCancelMsgField' }).props().value).toBe('');
+
+  //     wrapper.find({ testID: 'cancelGameFormCancelMsgField' }).props().onChangeText(cancelMsg);
+
+  //     expect(wrapper.state().cancelMsg).toBe(cancelMsg);
+
+  //     wrapper.find({ testID: 'cancelGameFormSubmitButton' }).props().onPress();
+
+  //     expect(Alert.alert).toHaveBeenCalled();
+  //     Alert.alert.mock.calls[0][2][1].onPress();
+
+  //     expect(handleBefore).toBeCalled();
+  //     expect(handleClientCancel).not.toBeCalled();
+  //     expect(handleSuccess).toBeCalledWith(expect.objectContaining({ cancelMsg }));
+  //   });
+  // });
 });
