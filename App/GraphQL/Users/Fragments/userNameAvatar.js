4adc8ad84ebca418c3e65d9cf5abab8b6b9db852
Tom Klaver <tomklav@gmail.com>
2019-02-20 11:18:24 +0100

    Single name GraphQL User queries

--- App/GraphQL/Users/Fragments/userNameAvatar.js
+++ App/GraphQL/Users/Fragments/userNameAvatar.js
@@ -2,8 +2,7 @@ import gql from 'graphql-tag';
 
 const userNameAvatarFragment = gql`
   fragment userNameAvatarFragment on UserType {
-    first_name
-    last_name
+    name
     profile {
       id
       uuid
