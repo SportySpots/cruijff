e70039abb7b3e754199dd450ed3becadd1cc09ab
federodes <federodes@gmail.com>
2019-03-01 00:36:58 +0100

    adding stories to ConnectionCheck

--- storybook/storyLoader.js
+++ storybook/storyLoader.js
@@ -19,6 +19,7 @@ function loadStories() {
   require('../App/Components/Common/CapacityPickerField/stories');
   require('../App/Components/Common/CenteredActivityIndicator/stories');
   require('../App/Components/Common/Checkbox/stories');
+  require('../App/Components/Common/ConnectionCheck/stories');
   require('../App/Components/Common/DarkFooter/stories');
   require('../App/Components/Common/DarkFooterButton/stories');
   require('../App/Components/Common/DatePickerField/stories');
@@ -143,6 +144,7 @@ const stories = [
   '../App/Components/Common/CapacityPickerField/stories',
   '../App/Components/Common/CenteredActivityIndicator/stories',
   '../App/Components/Common/Checkbox/stories',
+  '../App/Components/Common/ConnectionCheck/stories',
   '../App/Components/Common/DarkFooter/stories',
   '../App/Components/Common/DarkFooterButton/stories',
   '../App/Components/Common/DatePickerField/stories',
