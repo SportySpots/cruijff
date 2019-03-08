f5c0a29bd6cfe8e53e9acc7a9001acbfe0e64c77
Tom Klaver <tomklav@gmail.com>
2019-03-08 10:17:26 +0100

    Catch console.logs for DebugScreen also in __DEV__ mode

--- App/config.js
+++ App/config.js
@@ -33,33 +33,34 @@ if (__DEV__) {
   // If ReactNative's yellow box warnings are too much, it is possible to turn
   // it off, but the healthier approach is to fix the warnings.  =)
   // console.disableYellowBox = false;
-} else {
-  const bootTime = new Date();
-  const oldConsoleLog = console.log;
-  console.log = (...args) => {
-    oldConsoleLog(...args);
-    log.push({ logTime: (new Date() - bootTime) / 1000, ...args });
-  };
 }
+
+const bootTime = new Date();
+const oldConsoleLog = console.log;
+console.log = (...args) => {
+  oldConsoleLog(...args);
+  log.push({ logTime: (new Date() - bootTime) / 1000, ...args });
+};
+
 
 if (Config.ENVIRONMENT === 'TOM') {
   /* Fast overrides for Tom :) */
 
-  // settings.seedorfRestUrl = 'https://api.sportyspots.com/api';
+  settings.seedorfRestUrl = 'https://api.sportyspots.com/api';
   // settings.seedorfRestUrl = 'https://tom-dev.ngrok.io/api';
-  // settings.seedorfGraphQLUrl = 'https://api.sportyspots.com/graphql';
+  settings.seedorfGraphQLUrl = 'https://api.sportyspots.com/graphql';
   // settings.seedorfGraphQLUrl = 'https://tom-dev.ngrok.io/graphql';
   // settings.seedorfRestUrl = 'https://training.sportyspots.com/api';
   // settings.seedorfGraphQLUrl = 'https://training.sportyspots.com/graphql';
-  settings.seedorfRestUrl = 'http://10.0.2.2:8000/api';
-  settings.seedorfGraphQLUrl = 'http://10.0.2.2:8000/graphql';
+  // settings.seedorfRestUrl = 'http://10.0.2.2:8000/api';
+  // settings.seedorfGraphQLUrl = 'http://10.0.2.2:8000/graphql';
   // settings.testHostUrl = 'ws://10.0.2.2:8020';
 
   // settings.seedorfRestUrl = 'http://localhost:8000/api';
   // settings.seedorfGraphQLUrl = 'http://localhost:8000/graphql';
   // settings.testHostUrl = 'ws://localhost:8020';
-  settings.logRoute = true;
-  settings.logGraphQLQueries = true;
+  // settings.logRoute = true;
+  // settings.logGraphQLQueries = true;
   settings.testBuild = false;
 }
 
