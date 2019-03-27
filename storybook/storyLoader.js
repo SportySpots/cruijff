e4b96c53265995cbd5d7737a7826d9f2062cb41e
federodes <federodes@gmail.com>
2019-03-27 16:36:04 +0100

    fixing SpotImage stories

--- storybook/storyLoader.js
+++ storybook/storyLoader.js
@@ -100,9 +100,9 @@ function loadStories() {
   // require('../App/Components/Profile/UserMenu/stories');
   // require('../App/Components/Profile/UserSpots/stories');
   // require('../App/Components/Spots/BackgroundImage/stories');
-  require('../App/Components/Spots/SpotAmenities/stories');
+  // require('../App/Components/Spots/SpotAmenities/stories');
   // require('../App/Components/Spots/SpotHeader/stories');
-  // require('../App/Components/Spots/SpotImage/stories');
+  require('../App/Components/Spots/SpotImage/stories');
   // require('../App/Components/Spots/SpotLink/stories');
   // require('../App/Components/Spots/SpotListCard/stories');
   // require('../App/Components/Spots/SpotListCardSmall/stories');
@@ -230,9 +230,9 @@ const stories = [
   // '../App/Components/Profile/UserMenu/stories',
   // '../App/Components/Profile/UserSpots/stories',
   // '../App/Components/Spots/BackgroundImage/stories',
-  '../App/Components/Spots/SpotAmenities/stories',
+  // '../App/Components/Spots/SpotAmenities/stories',
   // '../App/Components/Spots/SpotHeader/stories',
-  // '../App/Components/Spots/SpotImage/stories',
+  '../App/Components/Spots/SpotImage/stories',
   // '../App/Components/Spots/SpotLink/stories',
   // '../App/Components/Spots/SpotListCard/stories',
   // '../App/Components/Spots/SpotListCardSmall/stories',
