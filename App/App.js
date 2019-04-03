d22c5d0335d24977bec0a1fc766caef76cd18c3e
federodes <federodes@gmail.com>
2019-04-03 11:47:43 +0200

    adding styled-components theme provider

--- App/App.js
+++ App/App.js
@@ -9,7 +9,7 @@ import { ApolloProvider } from 'react-apollo';
 import { StatusBar } from 'react-native';
 import firebase from 'react-native-firebase';
 import { MenuProvider } from 'react-native-popup-menu';
-import styled from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
 import config from './config';
 import client from './GraphQL/ApolloClient';
 // import mockClient from './GraphQL/ApolloMockClient';
@@ -22,9 +22,22 @@ import { SpotFiltersProvider } from './Context/SpotFilters';
 import { Events, getInitialEvent, IncomingLinks } from './Services/IncomingLinks';
 import globalRefs, { addGlobalRef } from './globalRefs';
 
-import Colors from './Themes/Colors';
+import scTheme from './Themes/scTheme'; // styled-components theme
 import { logNavigationState } from './utils';
 
+//------------------------------------------------------------------------------
+// STYLE:
+//------------------------------------------------------------------------------
+const AppRootView = styled.View`
+  flex: 1;
+  flex-direction: column;
+  background-color: ${({ theme }) => theme.colors.black};
+  margin-bottom: ${getBottomSpace()}px;
+  margin-top: ${ifIphoneX() ? 30 : 0}px;
+`;
+//------------------------------------------------------------------------------
+// COMPONENT:
+//------------------------------------------------------------------------------
 class App extends Component {
   constructor() {
     super();
@@ -127,44 +140,38 @@ class App extends Component {
         // client={config.useFixtures ? mockClient : client} // TODO
         client={client}
       >
-        <UserProvider>
-          <SpotFiltersProvider>
-            <LocationProvider>
-              <MenuProvider>
-                <AppRootView>
-                  <StatusBar barStyle="light-content" />
-                  <ConnectionCheck />
-                  <AppNavigation
-                    ref={(ref) => {
-                      this.router = ref;
-                      globalRefs.rootNavigator = ref;
-                    }}
-                    // See: https://reactnavigation.org/docs/en/screen-tracking.html
-                    onNavigationStateChange={(prevState, currState) => {
-                      if (config.logRoute) logNavigationState();
-                      const currScreen = getActiveRouteName(currState);
-                      const prevScreen = getActiveRouteName(prevState);
-                      if (prevScreen !== currScreen) {
-                        firebase.analytics().setCurrentScreen(currScreen);
-                      }
-                    }}
-                  />
-                </AppRootView>
-              </MenuProvider>
-            </LocationProvider>
-          </SpotFiltersProvider>
-        </UserProvider>
+        <ThemeProvider theme={scTheme}>
+          <UserProvider>
+            <SpotFiltersProvider>
+              <LocationProvider>
+                <MenuProvider>
+                  <AppRootView>
+                    <StatusBar barStyle="light-content" />
+                    <ConnectionCheck />
+                    <AppNavigation
+                      ref={(ref) => {
+                        this.router = ref;
+                        globalRefs.rootNavigator = ref;
+                      }}
+                      // See: https://reactnavigation.org/docs/en/screen-tracking.html
+                      onNavigationStateChange={(prevState, currState) => {
+                        if (config.logRoute) logNavigationState();
+                        const currScreen = getActiveRouteName(currState);
+                        const prevScreen = getActiveRouteName(prevState);
+                        if (prevScreen !== currScreen) {
+                          firebase.analytics().setCurrentScreen(currScreen);
+                        }
+                      }}
+                    />
+                  </AppRootView>
+                </MenuProvider>
+              </LocationProvider>
+            </SpotFiltersProvider>
+          </UserProvider>
+        </ThemeProvider>
       </ApolloProvider>
     );
   }
 }
-
-const AppRootView = styled.View`
-  flex: 1;
-  flex-direction: column;
-  background-color: ${Colors.black};
-  margin-bottom: ${getBottomSpace()}px;
-  margin-top: ${ifIphoneX() ? 30 : 0}px;
-`;
 
 export default codePush(App);
