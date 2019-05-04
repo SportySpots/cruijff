e296c3e3911bc1390e8b941cf3975a255d58df5a
federodes <federodes@gmail.com>
2019-05-04 19:38:52 +0200

    style for bubble username and chatComposer

--- App/Components/Chat/ChatBubble/index.js
+++ App/Components/Chat/ChatBubble/index.js
@@ -36,9 +36,18 @@ const timeTextStyle = {
     color: Colors.white,
   },
 };
+const bottomContainerStyle = {
+  left: {
+    justifyContent: 'space-between',
+  },
+  right: {
+    justifyContent: 'space-between',
+  },
+};
 const usernameStyle = {
   ...Fonts.SSM,
   color: Colors.link,
+  top: 0,
 };
 //------------------------------------------------------------------------------
 // COMPONENT:
@@ -50,6 +59,7 @@ const ChatBubble = props => (
     wrapperStyle={wrapperStyle}
     textStyle={textStyle}
     timeTextStyle={timeTextStyle}
+    bottomContainerStyle={bottomContainerStyle}
     usernameStyle={usernameStyle}
   />
 );
@@ -59,98 +69,3 @@ ChatBubble.propTypes = {
 };
 
 export default ChatBubble;
-
-
-// import React from 'react';
-// import { View } from 'react-native';
-// import { Bubble } from 'react-native-gifted-chat';
-// import Fonts from '../../../Themes/Fonts';
-// import Colors from '../../../Themes/Colors';
-// import Row from '../../Common/Row';
-// import Spacer from '../../Common/Spacer';
-// import Triangle from '../Triangle';
-
-// //------------------------------------------------------------------------------
-// // STYLE:
-// //------------------------------------------------------------------------------
-// const wrapperStyle = {
-//   left: {
-//     backgroundColor: Colors.white,
-//     borderRadius: 8,
-//   },
-//   right: {
-//     backgroundColor: Colors.notify,
-//     borderRadius: 8,
-//   },
-// };
-// const textStyle = {
-//   left: {
-//     fontFamily: Fonts.SM.fontFamily,
-//     fontSize: Fonts.SM.fontSize,
-//     color: Colors.black,
-//   },
-//   right: {
-//     fontFamily: Fonts.SM.fontFamily,
-//     fontSize: Fonts.SM.fontSize,
-//     color: Colors.black,
-//   },
-// };
-// const timeTextStyle = {
-//   left: {
-//     fontFamily: Fonts.SSM.fontFamily,
-//     fontSize: Fonts.SSM.fontSize,
-//     color: Colors.link,
-//   },
-//   right: {
-//     fontFamily: Fonts.SSM.fontFamily,
-//     fontSize: Fonts.SSM.fontSize,
-//     color: Colors.white,
-//   },
-// };
-// //------------------------------------------------------------------------------
-// // COMPONENT:
-// //------------------------------------------------------------------------------
-// // See: https://github.com/FaridSafi/react-native-gifted-chat/blob/master/src/Bubble.tsx
-// const ChatBubble = (props) => {
-//   const { position } = props;
-
-//   return (
-//     <Row
-//       justifyContent={position === 'left' ? 'flex-start' : 'flex-end'}
-//       style={{ flex: 1 }}
-//     >
-//       {position === 'left' && (
-//         <View>
-//           <Spacer size="L" />
-//           <Triangle
-//             position="top-right"
-//             primary={false}
-//           />
-//         </View>
-//       )}
-//       <Bubble
-//         {...props}
-//         wrapperStyle={wrapperStyle}
-//         textStyle={textStyle}
-//         timeTextStyle={timeTextStyle}
-//         // containerToNextStyle={containerToNextStyle}
-//         // containerToPreviousStyle={containerToPreviousStyle}
-//       />
-//       {position === 'right' && (
-//         <View>
-//           <Spacer size="L" />
-//           <Triangle
-//             position="top-left"
-//             primary
-//           />
-//         </View>
-//       )}
-//     </Row>
-//   );
-// };
-
-// ChatBubble.propTypes = {
-//   ...Bubble.propTypes,
-// };
-
-// export default ChatBubble;
