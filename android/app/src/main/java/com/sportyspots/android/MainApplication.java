f1b19f6a890f8fce05e92b989c96a0b088963e0b
Tom Klaver <tomklav@gmail.com>
2019-05-08 15:22:38 +0200

    ran react-native link react-native-gesture-handler

--- android/app/src/main/java/com/sportyspots/android/MainApplication.java
+++ android/app/src/main/java/com/sportyspots/android/MainApplication.java
@@ -3,6 +3,7 @@ package com.sportyspots.android;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.reactcommunity.rnlocalize.RNLocalizePackage;
 
 import io.invertase.firebase.RNFirebasePackage;
@@ -47,6 +48,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNGestureHandlerPackage(),
           new RNLocalizePackage(),
           new RNFirebasePackage(),
           new RNFirebaseAnalyticsPackage(),
