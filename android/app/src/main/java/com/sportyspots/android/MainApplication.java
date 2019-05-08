43b73338aea930d8f6adb023771db4bdea45a2e5
Tom Klaver <tomklav@gmail.com>
2019-05-08 15:32:41 +0200

    upgrade to play service 4.2.0 and ignore version check (react-native-code-push is incompatible)

--- android/app/src/main/java/com/sportyspots/android/MainApplication.java
+++ android/app/src/main/java/com/sportyspots/android/MainApplication.java
@@ -48,7 +48,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNGestureHandlerPackage(),
+          new RNGestureHandlerPackage(),
           new RNLocalizePackage(),
           new RNFirebasePackage(),
           new RNFirebaseAnalyticsPackage(),
