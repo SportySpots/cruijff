b4781240f45373e18c46446e85992d0e8aaa02bf
Tom Klaver <tomklav@gmail.com>
2019-05-08 17:32:16 +0200

    fixes storybook tests

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ module.exports = {
   // presets: ['@babel/preset-env', '@babel/preset-react'],
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
-    ['@babel/plugin-proposal-class-properties', { loose: true }],
-    '@babel/transform-runtime',
+    // ['@babel/plugin-proposal-class-properties', { loose: true }],
+    // '@babel/transform-runtime',
   ],
 };
