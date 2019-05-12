ae28713748f3faac955e845930f5ab2a5f0f00c8
federodes <federodes@gmail.com>
2019-05-12 11:39:52 +0200

    updating Slider dep

--- android/app/src/main/java/com/sportyspots/android/MainApplication.java
+++ android/app/src/main/java/com/sportyspots/android/MainApplication.java
@@ -3,6 +3,7 @@ package com.sportyspots.android;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.slider.ReactSliderPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.reactnativecommunity.netinfo.NetInfoPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -50,6 +51,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactSliderPackage(),
             new AsyncStoragePackage(),
             new NetInfoPackage(),
           new RNGestureHandlerPackage(),
