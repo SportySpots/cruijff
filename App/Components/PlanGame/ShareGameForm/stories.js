6ca905571c2b0ed8ea79f54f3d34f72340779957
federodes <federodes@gmail.com>
2019-05-29 17:30:12 +0200

    passing game prop to ShareGameForm instead of gameUUID and shareLink

--- App/Components/PlanGame/ShareGameForm/stories.js
+++ App/Components/PlanGame/ShareGameForm/stories.js
@@ -1,11 +1,22 @@
 import { storiesOf } from '@storybook/react-native';
 import React from 'react';
 import { View } from 'react-native';
+import { Query } from 'react-apollo';
+import GET_GAME_DETAILS from '../../../GraphQL/Games/Queries/GET_GAME_DETAILS';
 import ShareGameForm from '.';
 
 storiesOf('PlanGame.ShareGameForm', module)
   .add('ShareGameForm white theme', () => (
-    <View style={{ flex: 1 }}>
-      <ShareGameForm gameUUID="455" shareLink="http://some-link" />
-    </View>
+    <Query
+      query={GET_GAME_DETAILS}
+      variables={{ uuid: '455' }}
+    >
+      {({ loading, error, data }) => (
+        loading || error ? null : (
+          <View style={{ flex: 1 }}>
+            <ShareGameForm game={data.game} />
+          </View>
+        ))
+      }
+    </Query>
   ));
