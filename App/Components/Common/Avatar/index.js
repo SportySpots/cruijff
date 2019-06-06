15a73ad76190b633d955fe39b5d510c6dc30d190
federodes <federodes@gmail.com>
2019-06-06 19:25:13 +0200

    adding border radius to avatar

--- App/Components/Common/Avatar/index.js
+++ App/Components/Common/Avatar/index.js
@@ -26,6 +26,7 @@ const Circle = styled.View`
 const StyledImage = styled.Image`
   width: ${setSize}px;
   height: ${setSize}px;
+  border-radius: ${setSize}px;
 `;
 //------------------------------------------------------------------------------
 // COMPONENT:
