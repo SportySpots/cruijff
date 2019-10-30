5278e6cc9f49743d670970b9545502c11766d015
Tom Klaver <tomklav@gmail.com>
2019-10-30 15:07:33 +0100

    cleanup App imports

--- App/App.tsx
+++ App/App.tsx
@@ -1,32 +1,31 @@
 /* eslint-disable no-underscore-dangle */
 import React, { Component } from 'react';
-import Crashes from 'appcenter-crashes';
-
-import codePush from 'react-native-code-push';
-import { ApolloProvider } from 'react-apollo';
-import { ApolloProvider as ApolloHooksProvider } from '@apollo/react-hooks';
-
-import { StatusBar } from 'react-native';
-import firebase from 'react-native-firebase';
-import { MenuProvider } from 'react-native-popup-menu';
-import styled, { ThemeProvider } from 'styled-components/native';
 import config from './config';
+import Crashes from 'appcenter-crashes';
+import codePush from 'react-native-code-push';
+import firebase from 'react-native-firebase';
 import client from './GraphQL/ApolloClient';
 import AppNavigation, { getActiveRouteName } from './Navigation/AppNavigation';
 import {
   NavigationActions,
   NavigationContainerComponent,
 } from 'react-navigation';
-import { getBottomSpace, ifIphoneX } from './iphoneHelpers';
-import { LocationProvider } from './Context/Location';
-import { UserProvider } from './Context/User';
-import { SpotFiltersProvider } from './Context/SpotFilters';
+import { logNavigationState } from './utils';
 import { Events, getInitialEvent, IncomingLinks } from './Services/IncomingLinks';
 
-import scTheme from './Themes/scTheme'; // styled-components theme
-import { logNavigationState } from './utils';
+import { MenuProvider } from 'react-native-popup-menu';
+import { LocationProvider } from './Context/Location';
+import { UserProvider } from './Context/User';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloProvider as ApolloHooksProvider } from '@apollo/react-hooks';
+import { SpotFiltersProvider } from './Context/SpotFilters';
 import { CodePushProvider } from './Context/CodePush';
 import { NotificationsProvider } from './Context/Notifications';
+
+import styled, { ThemeProvider } from 'styled-components/native';
+import { getBottomSpace, ifIphoneX } from './iphoneHelpers';
+import scTheme from './Themes/scTheme'; // styled-components theme
+import { StatusBar } from 'react-native';
 
 //------------------------------------------------------------------------------
 // STYLE:
@@ -89,7 +88,7 @@ class App extends Component<{}, {}> {
     getInitialEvent().then((event) => {
       if (event
         && event.type
-        && [Events.MAGIC_LINK_LOGIN, Events.GAME_OPENED].includes(event.type)
+        && ([Events.MAGIC_LINK_LOGIN, Events.GAME_OPENED] as string[]).includes(event.type)
       ) {
         IncomingLinks.emitEvent(event);
       }
